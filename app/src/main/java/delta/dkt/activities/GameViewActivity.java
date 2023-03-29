6452d38b71a63503fa48689a5977b53ca02c9a48
djamn <david@jamnig.net>
2023-03-29 22:33:49 +0200

    forced all activities to landscape mode

--- app/src/main/java/delta/dkt/activities/GameViewActivity.java
+++ app/src/main/java/delta/dkt/activities/GameViewActivity.java
@@ -2,6 +2,7 @@ package delta.dkt.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 import delta.dkt.R;
@@ -12,5 +13,6 @@ public class GameViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_view);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); // Force landscape screen at activity level
     }
 }
