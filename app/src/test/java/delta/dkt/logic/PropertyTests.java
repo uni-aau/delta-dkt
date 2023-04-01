d2489d7034aeae959832971b1d3ceed202e10833
Ozarks <45905689+Marty-Byrde@users.noreply.github.com>
2023-04-01 15:42:06 +0200

    refactor: externalizing rent calculation

--- app/src/test/java/delta/dkt/logic/PropertyTests.java
+++ app/src/test/java/delta/dkt/logic/PropertyTests.java
@@ -15,7 +15,7 @@ public class PropertyTests {
     Property dummy = null;
 
     @BeforeEach
-    void setup() {
+    void setup () {
         dummy = new Property(12, 100, 10, PropertyLevel.normal, 100);
     }
 
@@ -24,7 +24,7 @@ public class PropertyTests {
      * This test checks whether the owner of a property can be set.
      */
     @Test
-    void checkPropertyOwner() {
+    void checkPropertyOwner () {
         Assertions.assertNull(dummy.getOwner());
 
         Assertions.assertDoesNotThrow(() -> {
@@ -39,7 +39,7 @@ public class PropertyTests {
      * Checks whether a hotel can be bought, including testing for game rules (only 1 hotel can be bought).
      */
     @Test
-    void checkHotelAccessory() {
+    void checkHotelAccessory () {
         testAccessory(0, 0);
 
         dummy.addHotel(1000);
@@ -53,7 +53,7 @@ public class PropertyTests {
      * Checks whether a house can be added to a property and if the amount of houses is capped at 4.
      */
     @Test
-    void check_addHouse() {
+    void check_addHouse () {
         testAccessory(0, 0);
 
         // add house nr 1
@@ -82,7 +82,7 @@ public class PropertyTests {
      * Checks whether the transition from 4 houses to 1 hotel works as indentended
      */
     @Test
-    void check_houseTransition() {
+    void check_houseTransition () {
         // add 4 houses to the property
         check_addHouse();
 
@@ -97,7 +97,7 @@ public class PropertyTests {
      * Checks whether a house can be added to the property when there is a hotel.
      */
     @Test
-    void check_Invalid_addHouse_withHotel() {
+    void check_Invalid_addHouse_withHotel () {
         testAccessory(0, 0);
         dummy.addHotel(1000);
 
@@ -110,34 +110,66 @@ public class PropertyTests {
      * Checks whether the rent increases with the amount of houses and hotels, in regard to the property level (factor).
      */
     @Test
-    void checkNormalRentCalculation() {
-        Assertions.assertEquals(10, dummy.calculateRent());
+    void checkNormalRentCalculation () {
+        int base = 10;
+        double levelFactor = 1;
+
+        testRentCalculations(base, levelFactor);
+    }
+    
+
+    /**
+     * Calculates the expected rent of a propery
+     * @param base The base rent of the property
+     * @param levelFactor The factor of the property cheap (0.5), normal (1), premium (1.5)
+     * @param houses The amount of houses on the property
+     * @param hotel The amount of hotels on the property
+     * @return Returns the expected rent of the property
+     */
+    private int expectedRent (int base, double levelFactor, int houses, int hotel) {
+        int rent = base;
+
+        // add house rent calculation
+        rent += houses * base * levelFactor;
+
+        // add hotel rent calculation
+        rent += hotel * base * levelFactor;
+
+        return rent;
+    }
+
+    /**
+     * This method is testing the different rent calculations for a property 0 - 4 houses and 0 - 1 hotel, based on the base rent and the level-factor of the property.
+     * @param base The base rent of the property
+     * @param levelFactor The factor of the property (0.5 | 1 | 1.5)
+     */
+    private void testRentCalculations (int base, double levelFactor) {
+        Assertions.assertEquals(expectedRent(base, levelFactor, 0, 0), dummy.calculateRent()); // => base rent of the property
 
         dummy.addHouse(1000);
-        Assertions.assertEquals(20, dummy.calculateRent());
+        Assertions.assertEquals(expectedRent(base, levelFactor, 1, 0), dummy.calculateRent());
 
         dummy.addHouse(1000);
-        Assertions.assertEquals(30, dummy.calculateRent());
+        Assertions.assertEquals(expectedRent(base, levelFactor, 2, 0), dummy.calculateRent());
 
         dummy.addHouse(1000);
-        Assertions.assertEquals(40, dummy.calculateRent());
+        Assertions.assertEquals(expectedRent(base, levelFactor, 3, 0), dummy.calculateRent());
 
         dummy.addHouse(1000);
-        Assertions.assertEquals(50, dummy.calculateRent());
+        Assertions.assertEquals(expectedRent(base, levelFactor, 4, 0), dummy.calculateRent());
 
 
         dummy.addHotel(2000);
         testAccessory(0, 1);
-        Assertions.assertEquals(70, dummy.calculateRent());
+        Assertions.assertEquals(expectedRent(base, levelFactor, 0, 1), dummy.calculateRent());
     }
 
     /**
      * Checks whether the properties accessories match the expected values.
-     *
      * @param expectedHouse The expected count of houses.
      * @param expectedHotel The expected count of hotels.
      */
-    private void testAccessory(int expectedHouse, int expectedHotel) {
+    private void testAccessory (int expectedHouse, int expectedHotel) {
         Assertions.assertEquals((expectedHouse + expectedHotel), dummy.getAccessories().size());
         Assertions.assertEquals(expectedHouse, dummy.getHouses());
         Assertions.assertEquals(expectedHotel, dummy.getHotels());
