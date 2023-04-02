5e9ae7097c77d2bdf57c5bc60939db6dc491032d
Ozarks <45905689+Marty-Byrde@users.noreply.github.com>
2023-04-02 17:20:02 +0200

    test: testing player's nickname

--- app/src/test/java/delta/dkt/logic/PlayerTests.java
+++ app/src/test/java/delta/dkt/logic/PlayerTests.java
@@ -3,7 +3,9 @@ package delta.dkt.logic;
 import delta.dkt.logic.structure.MapHandling;
 import delta.dkt.logic.structure.Player;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 
 class PlayerTests {
@@ -15,5 +17,14 @@ class PlayerTests {
     void setup () {
         player = new Player("Mike");
     }
+
+    /**
+     * Checks whether the initialized nickname, via the constructor, has been set and is accesible.
+     */
+    @Test
+    void checkNickname () {
+        assertEquals("Mike", player.getNickname());
+    }
     
+
 }
