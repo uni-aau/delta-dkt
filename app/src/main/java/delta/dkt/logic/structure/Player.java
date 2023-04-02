135c48269c8845c7647dd6096c67fe9ff38c20f8
Ozarks <45905689+Marty-Byrde@users.noreply.github.com>
2023-04-02 11:46:44 +0200

    feat: added variables & constructor

--- app/src/main/java/delta/dkt/logic/structure/Player.java
+++ app/src/main/java/delta/dkt/logic/structure/Player.java
@@ -1,5 +1,23 @@
 package delta.dkt.logic.structure;
 
-public class Player {
+import java.util.ArrayList;
 
+public class Player {
+    static int startCash = 500;
+    private static int _id = 1;
+
+    //? May be used to sync player data across clients
+    private int id = Player._id++;
+    private String nickname;
+
+    private Field location = null; //todo -> set this to be the start field
+    private int cash = Player.startCash;
+    private ArrayList<Property> properties = new ArrayList<>();
+
+    //? May be used to check whether a player is timeoutet, e.g. prison, or not.
+    private int suspention = 0;
+
+    public Player (String nickname) {
+        this.nickname = nickname;
+    }
 }
