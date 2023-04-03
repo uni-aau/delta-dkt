9b73ca62871414d9a6827c891aa5da9946c4b068
Anja81 <anja.adamitsch@gmx.at>
2023-04-04 01:07:50 +0200

    feat: added the classes PropertyHandler, TaskHandler and Task

---
+++ app/src/main/java/delta/dkt/logic/structure/Task.java
@@ -0,0 +1,11 @@
+package delta.dkt.logic.structure;
+
+public class Task {
+    public int id;
+        public String name;
+
+    public Task(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+}
