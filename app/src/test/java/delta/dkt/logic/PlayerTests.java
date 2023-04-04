42530516cbbf4bc2e877e7720184ec198c4a6016
Ozarks <45905689+Marty-Byrde@users.noreply.github.com>
2023-04-04 11:48:23 +0200

    test: testing successful property acquisition
    Checking whether the property a player is standing on can be bought and if the corresponding relations are set correctly.

--- app/src/test/java/delta/dkt/logic/PlayerTests.java
+++ app/src/test/java/delta/dkt/logic/PlayerTests.java
@@ -1,21 +1,24 @@
 package delta.dkt.logic;
 
-import delta.dkt.logic.structure.MapHandling;
-import delta.dkt.logic.structure.Player;
+import delta.dkt.logic.structure.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 class PlayerTests {
 
-    MapHandling mockMapHandling = mock(MapHandling.class);
+    GameMap mockMapHandling = mock(GameMap.class);
     Player player = null;
+
+    Property dummy = null;
 
     @BeforeEach
     void setup () {
         player = new Player("Mike");
+        dummy = new Property(12, 10, 10, PropertyLevel.NORMAL, 10, 10);
     }
 
     /**
@@ -25,6 +28,29 @@ class PlayerTests {
     void checkNickname () {
         assertEquals("Mike", player.getNickname());
     }
-    
+
+    /**
+     * Checks whether the property on which the player is standing on can be bought.
+     */
+    @Test
+    void checkBuyProperty_onLocation () {
+        Game.map = mockMapHandling;
+        Player.START_CASH = 1000;
+        when(mockMapHandling.getField(0)).thenReturn(dummy);
+        when(mockMapHandling.getField(dummy.getLocation())).thenReturn(dummy);
+
+        player = new Player("Patrick"); // => usage mock in instance variables
+
+        boolean paymentState = player.buyProperty();
+        assertTrue(paymentState);
+
+        assertEquals(1, player.getProperties().size());
+        assertEquals(player, player.getProperties().get(0).getOwner());
+        assertEquals((Player.START_CASH - dummy.getPrice()), player.getCash());
+
+        verify(mockMapHandling).getField(0);
+        verify(mockMapHandling).getField(dummy.getLocation());
+    }
+
 
 }
