95dbbaf28be02a22cef4cdda291eeaf091de35c4
Ozarks <45905689+Marty-Byrde@users.noreply.github.com>
2023-04-04 14:17:07 +0200

    fix: updating the preliminary parameter use

--- app/src/main/java/delta/dkt/logic/structure/GameMap.java
+++ app/src/main/java/delta/dkt/logic/structure/GameMap.java
@@ -3,7 +3,7 @@ package delta.dkt.logic.structure;
 public class GameMap {
 
     public Field getField (int location) {
-        System.out.println("Now i am using the location parameter that will be used when implementing this method!" + location);
+        System.err.println("Now i am using the location parameter that will be used when implementing this method!" + location);
 
         return null;
     }
