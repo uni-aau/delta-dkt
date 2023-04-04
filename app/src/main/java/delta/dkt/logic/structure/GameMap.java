6d6ac1cc29f2a5b337c3c09a8a8a1f97961eeb88
Ozarks <45905689+Marty-Byrde@users.noreply.github.com>
2023-04-04 14:31:30 +0200

    fix: replaced System prints with Logcat

--- app/src/main/java/delta/dkt/logic/structure/GameMap.java
+++ app/src/main/java/delta/dkt/logic/structure/GameMap.java
@@ -1,10 +1,11 @@
 package delta.dkt.logic.structure;
 
+import android.util.Log;
+
 public class GameMap {
 
     public Field getField (int location) {
-        System.err.println("Now i am using the location parameter that will be used when implementing this method!" + location);
-
+        Log.d("Todo", "I am using the location parameter, which will be used when this method is implemented, to get rid of the code smell." + location);
         return null;
     }
 }
