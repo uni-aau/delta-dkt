bd9880917c5a963953a150d1ab379b763ce50126
Ozarks <45905689+Marty-Byrde@users.noreply.github.com>
2023-04-04 12:23:31 +0200

    refactor: added parameterized test
    This test will check for the acquisition of a property when the player is on top of it and when he is not.

--- app/src/test/java/delta/dkt/logic/PlayerTests.java
+++ app/src/test/java/delta/dkt/logic/PlayerTests.java
@@ -3,6 +3,8 @@ package delta.dkt.logic;
 import delta.dkt.logic.structure.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -13,12 +15,14 @@ class PlayerTests {
     GameMap mockMapHandling = mock(GameMap.class);
     Player player = null;
 
-    Property dummy = null;
+    Property testProperty1 = null;
+    Property testProperty2 = null;
 
     @BeforeEach
     void setup () {
         player = new Player("Mike");
-        dummy = new Property(12, 10, 10, PropertyLevel.NORMAL, 10, 10);
+        testProperty1 = new Property(12, 10, 10, PropertyLevel.NORMAL, 10, 10);
+        testProperty2 = new Property(14, 10, 10, PropertyLevel.NORMAL, 10, 10);
     }
 
     /**
@@ -31,19 +35,25 @@ class PlayerTests {
 
 
     /**
-     * Checks whether the property, the player is standing on can be bought successfully, including the references that are being made during this cycle.
+     * Checks whether a property can be bought when standing on top of it and when a player would choose to buy any (free) property.
+     * Including reference checks included in this payment cycle.
      */
-    @Test
-    void checkBuyProperty_onLocation () {
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    void checkSuccessfulPropertyAcquisition (boolean onTop) {
         setMockRequirements_PropertyAquisition();
 
-        assertTrue(player.buyProperty());
+        Property buying = onTop ? testProperty1 : testProperty2;
 
-        assertEquals(1, player.getProperties().size());
-        assertEquals(player, player.getProperties().get(0).getOwner());
-        assertEquals((Player.START_CASH - dummy.getPrice()), player.getCash());
+        if (onTop) {
+            assertTrue(player.buyProperty());
+        } else {
+            assertTrue(player.buyProperty(testProperty2.getLocation()));
+        }
 
-        verifyMockCalls();
+        testSuccessfulPropertyAcquisition(1, buying.getPrice());
+
+        verifyMockCalls(buying);
     }
 
 
@@ -52,8 +62,9 @@ class PlayerTests {
      */
     void setMockRequirements_PropertyAquisition () {
         Game.map = mockMapHandling;
-        when(mockMapHandling.getField(0)).thenReturn(dummy);
-        when(mockMapHandling.getField(dummy.getLocation())).thenReturn(dummy);
+        when(mockMapHandling.getField(0)).thenReturn(testProperty1);
+        when(mockMapHandling.getField(testProperty1.getLocation())).thenReturn(testProperty1);
+        when(mockMapHandling.getField(testProperty2.getLocation())).thenReturn(testProperty2);
 
         player = new Player("Patrick"); // => usage mock in instance variables
     }
@@ -61,8 +72,19 @@ class PlayerTests {
     /**
      * This method will verify the calls of the previously mocked methods.
      */
-    void verifyMockCalls () {
+    void verifyMockCalls (Property bought) {
         verify(mockMapHandling).getField(0);
-        verify(mockMapHandling).getField(dummy.getLocation());
+        verify(mockMapHandling).getField(bought.getLocation());
+    }
+
+    /**
+     * This method will check whether the property has been added to the players collection,
+     * it will check if the owner property has been set correctly,
+     * it will check whether the player cash has been modified correctly.
+     */
+    void testSuccessfulPropertyAcquisition (int exProperties, int propertyPrice) {
+        assertEquals(exProperties, player.getProperties().size());
+        assertEquals(player, player.getProperties().get(0).getOwner());
+        assertEquals((Player.START_CASH - propertyPrice), player.getCash());
     }
 }
