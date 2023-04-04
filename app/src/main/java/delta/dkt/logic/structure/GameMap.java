fd7257501fba12536b803b478420c1619f088d1a
Ozarks <45905689+Marty-Byrde@users.noreply.github.com>
2023-04-04 14:42:05 +0200

    fix: removed Logcat
    Following the instructions of the code smells, leads to a failure in the build & analyse workflow.

--- app/src/main/java/delta/dkt/logic/structure/GameMap.java
+++ app/src/main/java/delta/dkt/logic/structure/GameMap.java
@@ -1,11 +1,9 @@
 package delta.dkt.logic.structure;
 
-import android.util.Log;
-
 public class GameMap {
 
     public Field getField (int location) {
-        Log.d("Todo", "I am using the location parameter, which will be used when this method is implemented, to get rid of the code smell." + location);
+        System.out.println("I am using the location parameter, which will be used when this method is implemented, to get rid of the code smell." + location);
         return null;
     }
 }
