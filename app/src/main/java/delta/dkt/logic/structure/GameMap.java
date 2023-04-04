239a59f124eac7723d9f1332ec08aab543ac539c
Ozarks <45905689+Marty-Byrde@users.noreply.github.com>
2023-04-04 14:12:22 +0200

    fix: preliminary use of parameter
    As this method is not yet implemented, the given parameter is also not in use. Hence, the parameter is used in a log statement to prevent this code smell.

--- app/src/main/java/delta/dkt/logic/structure/GameMap.java
+++ app/src/main/java/delta/dkt/logic/structure/GameMap.java
@@ -3,7 +3,8 @@ package delta.dkt.logic.structure;
 public class GameMap {
 
     public Field getField (int location) {
-        //todo to be implemented
+        System.out.println("Now i am using the location parameter that will be used when implementing this method!" + location);
+
         return null;
     }
 }
