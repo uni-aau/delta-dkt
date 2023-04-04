f0820ac228a7ab595459867f04e7c40b016fa4e4
Anja81 <anja.adamitsch@gmx.at>
2023-04-04 04:15:38 +0200

    feat: created the methods getRandomRiskTask und getRandomBankTask

--- app/src/main/java/delta/dkt/logic/structure/BankTaskCollection.java
+++ app/src/main/java/delta/dkt/logic/structure/BankTaskCollection.java
@@ -1,6 +1,7 @@
 package delta.dkt.logic.structure;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class BankTaskCollection {
     private ArrayList<BankTask> bankTask = new ArrayList<>();
@@ -8,6 +9,11 @@ public class BankTaskCollection {
     public BankTaskCollection(){
         bankTask.add(new BankTask(1,"BankCard1","Do something"));
         bankTask.add(new BankTask(2,"BankCard2", "Do another thing"));
+    }
+
+    public BankTask getRandomBankTask(){
+        Collections.shuffle(bankTask);
+        return bankTask.get(0);
     }
 
 }
--- app/src/main/java/delta/dkt/logic/structure/RiskTaskCollection.java
+++ app/src/main/java/delta/dkt/logic/structure/RiskTaskCollection.java
@@ -1,6 +1,7 @@
 package delta.dkt.logic.structure;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class RiskTaskCollection {
     private ArrayList<RiskTask> riskTask = new ArrayList<>();
@@ -8,5 +9,10 @@ public class RiskTaskCollection {
     public RiskTaskCollection(){
         riskTask.add(new RiskTask(1,"RiskCard1","Do something"));
         riskTask.add(new RiskTask(2,"RiskCard2","Do another thing"));
+    }
+
+    public RiskTask getRandomRiskTask(){
+        Collections.shuffle(riskTask);
+        return riskTask.get(0);
     }
 }
