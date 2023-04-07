b4a8a9d4f5b91eb4c894c8808e8b99e019e2165a
Matthaeus <m.gustab@hotmail.com>
2023-04-07 16:11:28 +0200

    feat: added Button Function to enter the next Activity

--- app/src/main/java/delta/dkt/activities/MainActivity.java
+++ app/src/main/java/delta/dkt/activities/MainActivity.java
@@ -2,17 +2,33 @@ package delta.dkt.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import delta.dkt.R;
 
 public class MainActivity extends AppCompatActivity {
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); // Force landscape screen at activity level
+        Button enter = findViewById(R.id.enter_btn);
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainMenuActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
+
 }
