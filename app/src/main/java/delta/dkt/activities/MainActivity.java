98493bb30efbbdc31557e51d0b6c427e100d047a
Matthaeus <m.gustab@hotmail.com>
2023-04-07 16:28:50 +0200

    feat: added Username function + toast Message feature

--- app/src/main/java/delta/dkt/activities/MainActivity.java
+++ app/src/main/java/delta/dkt/activities/MainActivity.java
@@ -8,22 +8,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import delta.dkt.R;
 
 public class MainActivity extends AppCompatActivity {
-    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText edtxt = findViewById(R.id.username_edittext);
         Button enter = findViewById(R.id.enter_btn);
+
         enter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), MainMenuActivity.class);
-                startActivity(intent);
+                String username = edtxt.getText().toString();
+                if(username.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Please enter Username", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "Welcome " + username + "!", Toast.LENGTH_SHORT).show();
+                    startActivity(intent);
+                }
             }
         });
 
