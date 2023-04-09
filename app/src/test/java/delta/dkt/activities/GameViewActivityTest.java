daa80e214d63560924e5decc4a10637e08bc87c2
djamn <david@jamnig.net>
2023-04-09 21:46:17 +0200

    added first testing case for gameview impl

--- app/src/test/java/delta/dkt/activities/GameViewActivityTest.java
+++ app/src/test/java/delta/dkt/activities/GameViewActivityTest.java
@@ -1,4 +1,35 @@
 package delta.dkt.activities;
 
+import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.*;
+
+import static org.mockito.Mockito.*;
+
+//@RunWith(JUnitPlatform.class)
 public class GameViewActivityTest {
+    @InjectMocks
+    private static GameViewActivity gameViewActivity;
+    @Mock
+    private AppCompatActivity mockActivity;
+
+    @BeforeEach
+    void setUp() {
+        gameViewActivity = new GameViewActivity();
+    }
+
+    @Test
+    public void testIntentSwitch() {
+        Intent expectedIntent = new Intent(mockActivity, PropertyListActivity.class);
+
+        gameViewActivity.switchToPropertyActivity();
+
+        verify(mockActivity).startActivity(expectedIntent);
+    }
 }
