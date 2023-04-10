dc6aa1f90a056bcbb21636429e2d6fb527fd6aa2
djamn <david@jamnig.net>
2023-04-10 14:45:22 +0200

    first impl. of blockfragment to receive and set params from the activity

--- app/src/main/java/delta/dkt/BlockFragment.java
+++ app/src/main/java/delta/dkt/BlockFragment.java
@@ -7,41 +7,25 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BlockFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class BlockFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_fragmentNumber = "fragmentNumberInput";
+    private static final String ARG_propNumber = "propNumberInput";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String fragmentNumberInput;
+    private String propNumberInput;
 
     public BlockFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BlockFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static BlockFragment newInstance(String param1, String param2) {
         BlockFragment fragment = new BlockFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_fragmentNumber, param1);
+        args.putString(ARG_propNumber, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,15 +34,28 @@ public class BlockFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            fragmentNumberInput = getArguments().getString(ARG_fragmentNumber);
+            propNumberInput = getArguments().getString(ARG_propNumber);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_block, container, false);
+
+        setFragmentData(view);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_block, container, false);
+        return view;
+    }
+
+    protected void setFragmentData(View view) {
+        TextView propNumber = view.findViewById(R.id.textview_propNumber);
+        TextView propName = view.findViewById(R.id.textView_propName);
+        TextView price = view.findViewById(R.id.textView_price);
+        TextView ownedBy = view.findViewById(R.id.textView_ownedBy);
+
+        propNumber.setText(propNumberInput);
     }
 }
