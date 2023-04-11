49f67f2ad9d6393b65e4fb0c73a198bdf6610e4e
Matthaeus <m.gustab@hotmail.com>
2023-04-11 15:03:19 +0200

    feat: created UsernameAdapter von RecyclerView

---
+++ app/src/main/java/delta/dkt/activities/UserNameAdapter.java
@@ -0,0 +1,29 @@
+package delta.dkt.activities;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+class UserNameAdapter extends RecyclerView.Adapter<UserNameAdapter.UserViewHolder> {
+
+
+
+    @NonNull
+    @Override
+    public UserNameAdapter.UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserNameAdapter.UserViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
