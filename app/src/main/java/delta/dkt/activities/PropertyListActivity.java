01730ac8712f6372834f0d1f4beb22f1e33bf746
djamn <david@jamnig.net>
2023-04-11 23:34:58 +0200

    added scrollbar and information creation via framelayouts

--- app/src/main/java/delta/dkt/activities/PropertyListActivity.java
+++ app/src/main/java/delta/dkt/activities/PropertyListActivity.java
@@ -23,9 +23,13 @@ public class PropertyListActivity extends AppCompatActivity {
 
     protected void sendDataToFragment() {
         BlockFragment blockNumber1 = BlockFragment.newInstance("Fragment1", "1", "Testprop", 5, "Player1", 2);
-//        BlockFragment blockNumber2 = BlockFragment.newInstance("Fragment2", "2", "Testprop2", 10, "Player2", 1);
+        BlockFragment blockNumber2 = BlockFragment.newInstance("Fragment2", "2", "Testprop2", 10, "Player2", 1);
+        BlockFragment blockNumber3 = BlockFragment.newInstance("Fragment3", "2", "Testprop2", 10, "Player2", 1);
+        BlockFragment blockNumber4 = BlockFragment.newInstance("Fragment4", "2", "Testprop2", 10, "Player2", 1);
 
         getSupportFragmentManager().beginTransaction().add(R.id.framelayout1, blockNumber1).commit();
-//        getSupportFragmentManager().beginTransaction().add(R.id.fragmentContainerView2, blockNumber2).commit();
+        getSupportFragmentManager().beginTransaction().add(R.id.framelayout2, blockNumber2).commit();
+        getSupportFragmentManager().beginTransaction().add(R.id.framelayout3, blockNumber3).commit();
+        getSupportFragmentManager().beginTransaction().add(R.id.framelayout4, blockNumber4).commit();
     }
 }
