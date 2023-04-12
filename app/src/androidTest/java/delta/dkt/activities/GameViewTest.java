aa5bd10bfb3ab684fcb66f8349b2d0cf0f85c864
djamn <david@jamnig.net>
2023-04-12 21:44:50 +0200

    added testing case for switchToPropertyInformation

--- app/src/androidTest/java/delta/dkt/activities/GameViewTest.java
+++ app/src/androidTest/java/delta/dkt/activities/GameViewTest.java
@@ -5,10 +5,16 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.matcher.IntentMatchers;
 
 import org.junit.Rule;
 import org.junit.Test;
+
+import delta.dkt.R;
 
 public class GameViewTest {
     @Rule
@@ -18,5 +24,16 @@ public class GameViewTest {
     @Test
     public void listGoesOverTheFold() {
         onView(withText("General Information")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testViewSwitch() {
+        Intents.init();
+        // Perform an action on the button
+        onView(ViewMatchers.withId(R.id.button_property_infos)).perform(ViewActions.click());
+
+        // Verify that the activity you expect to be launched has started
+        Intents.intended(IntentMatchers.hasComponent(PropertyListActivity.class.getName()));
+        Intents.release();
     }
 }
