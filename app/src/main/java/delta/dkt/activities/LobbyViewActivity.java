ea7ca05c75abfc6d3917f5699e9219757208251c
Matthaeus <m.gustab@hotmail.com>
2023-04-12 16:39:31 +0200

    fix: removed unused import

--- app/src/main/java/delta/dkt/activities/LobbyViewActivity.java
+++ app/src/main/java/delta/dkt/activities/LobbyViewActivity.java
@@ -3,7 +3,6 @@ package delta.dkt.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
