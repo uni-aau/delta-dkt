19482b8041468e8508d8c62a8c6658cdf3599233
Matthaeus <m.gustab@hotmail.com>
2023-04-13 18:10:29 +0200

    fix: added static final intentParameter to add to the Intents and avoid duplicates

--- app/src/main/java/delta/dkt/activities/MainActivity.java
+++ app/src/main/java/delta/dkt/activities/MainActivity.java
@@ -14,6 +14,7 @@ import delta.dkt.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String intentParameter = "username";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this, "Please enter Username", Toast.LENGTH_SHORT).show();
                 }else{
                     Toast.makeText(MainActivity.this, "Welcome " + username + "!", Toast.LENGTH_SHORT).show();
-                    intent.putExtra("stringname", username);
+                    intent.putExtra(intentParameter, username);
                     startActivity(intent);
                 }
             }
