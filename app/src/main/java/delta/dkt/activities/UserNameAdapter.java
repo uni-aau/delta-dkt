d8065ba9822d008221748752836340f0ad3f1528
Matthaeus <m.gustab@hotmail.com>
2023-04-13 17:30:01 +0200

    feat: created methods for userNameAdapter which is needed for the User Recycler View in the Lobby

--- app/src/main/java/delta/dkt/activities/UserNameAdapter.java
+++ app/src/main/java/delta/dkt/activities/UserNameAdapter.java
@@ -1,8 +1,10 @@
 package delta.dkt.activities;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -24,35 +26,21 @@ class UserNameAdapter extends RecyclerView.Adapter<UserNameAdapter.UserViewHolde
         this.usernames= usernames;
     }
 
-
-
-    class UserViewHolder extends RecyclerView.ViewHolder {
-        // Grabs informations thats needed from the recycler_view_user_row.xml
-
-        TextView tv_username;
-
-        public UserViewHolder (@NonNull View itemView) {
-            super(itemView);
-            tv_username = itemView.findViewById(R.id.userName_txt);
-        }
-    }
-
-
-
-
-
-
     @NonNull
     @Override
     public UserNameAdapter.UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         // This method inflates the layout, meaning giving a look to our rows
-        return null;
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.recycler_view_user_row, parent, false);
+        return new UserViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull UserNameAdapter.UserViewHolder holder, int position) {
         // assigning values to the views that come back on the screen again (in the recycler view layout file)
         // based on the position of the recycler view
+
+        holder.tv_username.setText(usernames.get(position));
 
 
     }
@@ -62,5 +50,23 @@ class UserNameAdapter extends RecyclerView.Adapter<UserNameAdapter.UserViewHolde
         // this method just returns the number of items you want to display
         // now its 1 because of testing purpose as we only have 1 user at the moment
         return usernames.size();
+    }
+
+
+
+
+
+
+    class UserViewHolder extends RecyclerView.ViewHolder {
+        // Grabs informations thats needed from the recycler_view_user_row.xml
+
+        TextView tv_username;
+        ImageView iv_avatar;
+
+        public UserViewHolder (@NonNull View itemView) {
+            super(itemView);
+            tv_username = itemView.findViewById(R.id.userName_txt);
+            iv_avatar = itemView.findViewById(R.id.avatar);
+        }
     }
 }
