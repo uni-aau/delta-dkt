f418e53037ad26d7b8b7f54ee5d2f1f64cf59aea
Matthaeus <m.gustab@hotmail.com>
2023-04-13 18:13:19 +0200

    fix: removed unused imports

--- app/src/main/java/delta/dkt/activities/MainActivity.java
+++ app/src/main/java/delta/dkt/activities/MainActivity.java
@@ -3,9 +3,7 @@ package delta.dkt.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
