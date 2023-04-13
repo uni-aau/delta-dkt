39dce23afc82ecde010c998468526fd8fb39ee7c
djamn <david@jamnig.net>
2023-04-13 23:04:54 +0200

    fix: removed unused import

--- app/src/main/java/delta/dkt/activities/MainMenuActivity.java
+++ app/src/main/java/delta/dkt/activities/MainMenuActivity.java
@@ -4,7 +4,6 @@ import static delta.dkt.activities.MainActivity.INTENT_PARAMETER;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
