ebee2845a49d44bf9f71ddd3f5afb6199002f195
Wolfgang Prett <wolfi@10.0.0.33>
2023-04-16 16:41:54 +0200

    removed printStackTrace

--- app/src/main/java/delta/dkt/activities/MainActivity.java
+++ app/src/main/java/delta/dkt/activities/MainActivity.java
@@ -60,7 +60,6 @@ public class MainActivity extends AppCompatActivity {
         try {
             Thread.sleep(200);
         } catch (InterruptedException e) {
-            e.printStackTrace();
             Log.e("INTERRUPT", "Interrupted!", e);
             Thread.currentThread().interrupt();
         }
