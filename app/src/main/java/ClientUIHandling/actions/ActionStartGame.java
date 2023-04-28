4416597149e75a3d33215df097a0c39826df61e9
djamn <david@jamnig.net>
2023-04-28 21:45:24 +0200

    feat: fixed type and added activity switch

--- app/src/main/java/ClientUIHandling/actions/ActionStartGame.java
+++ app/src/main/java/ClientUIHandling/actions/ActionStartGame.java
@@ -6,12 +6,16 @@ import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 
 import ClientUIHandling.ClientActionInterface;
+import ServerLogic.ServerActionHandler;
+import delta.dkt.activities.GameViewActivity;
 
 public class ActionStartGame implements ClientActionInterface {
     @Override
     public void execute(AppCompatActivity activity, String clientMessage) {
         Log.d("Client UI Action", "Successfully received action from server handler: Activity: " + activity + " Message: " + clientMessage);
-//        Intent switchToGameViewIntent = new Intent(activity);
+        Intent switchToGameViewIntent = new Intent(activity, GameViewActivity.class);
+        activity.startActivity(switchToGameViewIntent);
 
+        // TODO get unique ID
     }
 }
--- app/src/main/java/ServerLogic/actions/StartGameRequest.java
+++ app/src/main/java/ServerLogic/actions/StartGameRequest.java
@@ -1,6 +1,7 @@
 package ServerLogic.actions;
 
 import static ClientUIHandling.Constants.GameViewActivityType;
+import static ClientUIHandling.Constants.LobbyViewActivityType;
 import static ClientUIHandling.Constants.PREFIX_GAME_START;
 
 import android.util.Log;
@@ -14,7 +15,7 @@ public class StartGameRequest implements ServerActionInterface {
         Log.d("Server START GAME", "Start request received! Server: " + server + " parameters: " + parameters);
 
         String[] notificationMessage = {"Test"};
-        server.broadcast(GameViewActivityType, PREFIX_GAME_START, notificationMessage);
+        server.broadcast(LobbyViewActivityType, PREFIX_GAME_START, notificationMessage);
 //        server.broadcast(GameViewActivityType + ":"+ PREFIX_GAME_START + " Game started!");
         // TODO
     }
