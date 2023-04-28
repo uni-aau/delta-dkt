ee787dbe46a9388b589f52b031e52e41235f26d9
Ozarks <45905689+Marty-Byrde@users.noreply.github.com>
2023-04-28 12:00:21 +0200

    feat: modified the getFields method
    Now the fields are initialized once, when the Map is initialized and then fields are being stored internally.

--- app/src/main/java/delta/dkt/logic/structure/GameMap.java
+++ app/src/main/java/delta/dkt/logic/structure/GameMap.java
@@ -1,8 +1,10 @@
 package delta.dkt.logic.structure;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 public class GameMap {
+    private ArrayList<Field> fields = FieldHandler.getFields();
 
     public Field getField (int location) {
         System.out.println("I am using the location parameter, which will be used when this method is implemented, to get rid of the code smell." + location);
@@ -12,12 +14,10 @@ public class GameMap {
     }
 
     /**
-     * This method returns the all the fields on the game-map.
+     * This method returns all the fields on the map. Note that, fields are initialized once and then stored in a local variable inside the GameMap, to preserve the field-states, such as owners, houses and more.
+     * @return Returns the fields of the map.
      */
     public ArrayList<Field> getFields () {
-        ArrayList<Field> tmpFields = new ArrayList<>();
-        for (int i = 0; i < 40; i++) tmpFields.add(null);
-
-        return tmpFields;
+        return fields;
     }
 }
