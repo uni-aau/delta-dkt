c61486a595a8f64439a5c77f425efae16c6e4e27
Ozarks <45905689+Marty-Byrde@users.noreply.github.com>
2023-04-28 11:55:02 +0200

    refactor: applied code-styles

--- app/src/main/java/delta/dkt/logic/structure/FieldHandler.java
+++ app/src/main/java/delta/dkt/logic/structure/FieldHandler.java
@@ -4,18 +4,18 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 public class FieldHandler {
-
-    public static ArrayList<Field> getFields(){
+    
+    public static ArrayList<Field> getFields() {
         ArrayList<Field> fields = new ArrayList<>();
 
-        for(int i = 0; i < 40; i++){
-            if(PropertyHandler.getProperties(i) != null) fields.add(PropertyHandler.getProperties(i));
-            if(TaskHandler.getTask(i) != null) fields.add(TaskHandler.getTask(i));
+        for (int i = 0; i < 40; i++) {
+            if (PropertyHandler.getProperties(i) != null) fields.add(PropertyHandler.getProperties(i));
+            if (TaskHandler.getTask(i) != null) fields.add(TaskHandler.getTask(i));
 
-            if(i == 1) fields.add(new SpecialField(i, "Start"));
-            if(i == 11) fields.add(new SpecialField(i, "Gesetztes Verletzung"));
-            if(i == 21) fields.add(new SpecialField(i, "Vermögens Abgabe"));
-            if(i == 31) fields.add(new SpecialField(i, "Gefängnis"));
+            if (i == 1) fields.add(new SpecialField(i, "Start"));
+            if (i == 11) fields.add(new SpecialField(i, "Gesetztes Verletzung"));
+            if (i == 21) fields.add(new SpecialField(i, "Vermögens Abgabe"));
+            if (i == 31) fields.add(new SpecialField(i, "Gefängnis"));
         }
 
         return fields;
