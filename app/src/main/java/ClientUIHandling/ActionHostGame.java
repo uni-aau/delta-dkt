2f52a8b4e15792d99f6d9923416c15490a0b4c51
Matthaeus <m.gustab@hotmail.com>
2023-04-29 19:25:54 +0200

    feat: added Log.d message

--- app/src/main/java/ClientUIHandling/ActionHostGame.java
+++ app/src/main/java/ClientUIHandling/ActionHostGame.java
@@ -3,19 +3,23 @@ package ClientUIHandling;
 import static delta.dkt.activities.MainActivity.INTENT_PARAMETER;
 
 import android.content.Intent;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import delta.dkt.activities.LobbyViewActivity;
 import delta.dkt.activities.MainMenuActivity;
 
-public class ActionHostNewGame implements ClientActionInterface {
+public class ActionHostGame implements ClientActionInterface {
 
     @Override
     public void execute(AppCompatActivity activity, String clientMessage) {
 
+        Log.d("CLIENT_HOST", "Client is in da Lobby!!");
+
         MainMenuActivity mainMenuActivity = (MainMenuActivity) activity;
-        Intent intent = new Intent(mainMenuActivity, LobbyViewActivity.class);
+
+        Intent intent = new Intent(activity, LobbyViewActivity.class);
         intent.putExtra(INTENT_PARAMETER, "UserName");
         mainMenuActivity.startActivity(intent);
     }
