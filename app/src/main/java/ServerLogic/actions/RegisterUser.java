4d84675d56e666b2884bb09872933bdacad5bcc4
djamn <david@jamnig.net>
2023-05-01 01:12:35 +0200

    feat: added user register

--- app/src/main/java/ServerLogic/actions/RegisterUser.java
+++ app/src/main/java/ServerLogic/actions/RegisterUser.java
@@ -1,15 +1,27 @@
 package ServerLogic.actions;
 
+import static ClientUIHandling.Constants.GameViewActivityType;
+import static ClientUIHandling.Constants.LobbyViewActivityType;
+import static ClientUIHandling.Constants.PREFIX_ROLL_DICE_PERM;
+
 import android.util.Log;
 
 import ServerLogic.ServerActionInterface;
+import delta.dkt.activities.GameViewActivity;
+import delta.dkt.logic.structure.Game;
+import delta.dkt.logic.structure.Player;
 import network2.ServerNetworkClient;
 
 public class RegisterUser implements ServerActionInterface {
     @Override
     public void execute(ServerNetworkClient server, Object parameters) {
+        String uuid = (String) parameters;
         Log.d("Server Register User", "Register User request received! Server: " + server + " parameters: " + parameters);
 
-        // TODO
+        int size = Game.getPlayers().size();
+        System.out.println("Debug - Gamesize: " + size);
+        Game.getPlayers().put(size + 1, new Player(uuid));
+
+        // Todo start toast + update online players + button enabling
     }
 }
