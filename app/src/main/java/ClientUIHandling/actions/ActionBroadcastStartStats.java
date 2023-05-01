4fbbf22899b7fe94c4eb6b606c7833d79c9b9452
djamn <david@jamnig.net>
2023-05-01 02:21:37 +0200

    feat: added playeramount to textview

--- app/src/main/java/ClientUIHandling/actions/ActionBroadcastStartStats.java
+++ app/src/main/java/ClientUIHandling/actions/ActionBroadcastStartStats.java
@@ -1,6 +1,7 @@
 package ClientUIHandling.actions;
 
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +11,7 @@ import ClientUIHandling.Constants;
 import delta.dkt.R;
 
 public class ActionBroadcastStartStats implements ClientActionInterface {
+    private int MAX_PLAYER_VALUE = -1; // Todo muss via Lobby settings implementiert werden
     @Override
     public void execute(AppCompatActivity activity, String clientMessage) {
         String[] args = clientMessage.replace(Constants.PREFIX_GAME_START_STATS, "").trim().split(";"); // Holt sich Args nach dem Prefix
@@ -19,10 +21,11 @@ public class ActionBroadcastStartStats implements ClientActionInterface {
         String toastValue = String.format(activity.getString(R.string.start_broadcast_toast_text), args[0]);
         Toast.makeText(activity, toastValue, Toast.LENGTH_SHORT).show();
 
-        updateGeneralInfo();
+        updateGeneralInfo(activity, args[0]);
     }
 
-    private void updateGeneralInfo() {
-
+    private void updateGeneralInfo(AppCompatActivity activity, String playerAmount) {
+        String inputValue = String.format(activity.getString(R.string.players_online), Integer.parseInt(playerAmount), MAX_PLAYER_VALUE);
+        ((TextView)activity.findViewById(R.id.textView_players_online)).setText(inputValue);
     }
 }
