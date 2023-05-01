7c90aaf8d59f1811e5f1cacaa70c0a3c1613340d
Matthaeus <m.gustab@hotmail.com>
2023-05-01 16:47:16 +0200

    feat: added showServerPopUpWindow method. This method shows the user a window to enter the servername

--- app/src/main/java/delta/dkt/activities/MainMenuActivity.java
+++ app/src/main/java/delta/dkt/activities/MainMenuActivity.java
@@ -3,13 +3,24 @@ package delta.dkt.activities;
 import static ClientUIHandling.Constants.PREFIX_HOST_NEW_GAME;
 import static delta.dkt.activities.MainActivity.INTENT_PARAMETER;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.textfield.TextInputEditText;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -37,11 +48,7 @@ public class MainMenuActivity extends AppCompatActivity {
         String newUser = getIntent().getStringExtra(INTENT_PARAMETER);
 
 
-
-
-        host.setOnClickListener(view -> {
-            startServer();
-        });
+        host.setOnClickListener(view -> showServerPopUpWindow());
 
         join.setOnClickListener(view -> {
             Intent intent = new Intent(getApplicationContext(), LobbyViewActivity.class);
@@ -53,10 +60,37 @@ public class MainMenuActivity extends AppCompatActivity {
 
     }
 
-    public void startServer(){
+    public void showServerPopUpWindow() {
+
+        ConstraintLayout popUpConstraintLayout = findViewById(R.id.popUpConstraintLayout);
+        View view = LayoutInflater.from(MainMenuActivity.this).inflate(R.layout.server_name_pop_up_window, popUpConstraintLayout);
+
+        Button okButton = view.findViewById(R.id.okButton);
+        Button cancelButton = view.findViewById(R.id.cancelButton);
+        EditText editText = view.findViewById(R.id.popUpEditText);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainMenuActivity.this);
+        builder.setView(view);
+        final AlertDialog alertDialog = builder.create();
+
+        okButton.setOnClickListener(view1 -> {
+            String serverName = editText.getText().toString();
+            startServer(serverName);
+        });
+
+        cancelButton.setOnClickListener(view1 -> alertDialog.dismiss());
+
+        if (alertDialog.getWindow() != null) {
+            alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+        }
+        alertDialog.show();
+
+    }
+
+    public void startServer(String serverName){
         server = new ServerNetworkClient();
         server.start();
-        Toast.makeText(MainMenuActivity.this, "Server started on "+getTime(), Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainMenuActivity.this, "Server "+serverName+" started on "+getTime(), Toast.LENGTH_SHORT).show();
         ServerActionHandler.triggerAction(PREFIX_HOST_NEW_GAME, null);
     }
 
@@ -64,6 +98,8 @@ public class MainMenuActivity extends AppCompatActivity {
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
         return sdf.format(new Date());
     }
+
+
 
 }
 
