1d43a99c9f47dfb21bf0cb9a3d1de17ea9f35222
djamn <david@jamnig.net>
2023-05-01 16:59:38 +0200

    feat: prevent board touchevent when client id is not 1 (initially)

--- app/src/main/java/ClientUIHandling/actions/ActionRollDicePermission.java
+++ app/src/main/java/ClientUIHandling/actions/ActionRollDicePermission.java
@@ -24,14 +24,18 @@ public class ActionRollDicePermission implements ClientActionInterface {
         String[] args = clientMessage.replace(PREFIX_ROLL_DICE_PERM, "").trim().split(";");
         int clientId = Integer.parseInt(args[0]);
         Button rollDice = activity.findViewById(R.id.button_roll_dice);
+        ImageView gameBoard = activity.findViewById(R.id.imageView);
 
         Log.d("[UI] Roll Dice Perm", "Successfully received roll dice perm action from server handler: Activity: " + activity + " ClientID: " + clientId);
 
+        // Initial State (only client ID 1 can roll the dice)
         if (clientId == 1) {
             rollDice.setEnabled(true);
+            gameBoard.setEnabled(true);
             rollDice.setBackgroundResource(R.drawable.host_btn_background);
         } else {
-            rollDice.setEnabled(false);
+            rollDice.setEnabled(false); // prevent touch event
+            gameBoard.setEnabled(false);
             rollDice.setBackgroundResource(R.drawable.host_btn_background_disabled);
         }
 
@@ -43,6 +47,6 @@ public class ActionRollDicePermission implements ClientActionInterface {
         Log.d("[UI] Action", "Received client ID for player enabling: " + clientId);
 
         int figureIdentifier = activity.getResources().getIdentifier("player" + clientId, "id", activity.getPackageName());
-        activity.findViewById(figureIdentifier).setVisibility(View.VISIBLE); // Todo es wird trotzdem noch die alte ID sichtbar gemacht?
+        activity.findViewById(figureIdentifier).setVisibility(View.VISIBLE);
     }
 }
