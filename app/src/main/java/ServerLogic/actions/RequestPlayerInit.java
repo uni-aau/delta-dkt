7f399b91d2c59e1a74cb3120346ee36715ec002f
djamn <david@jamnig.net>
2023-05-03 10:48:39 +0200

    fix: renamed activity types to fix critical codesmells

--- app/src/main/java/ServerLogic/actions/RequestPlayerInit.java
+++ app/src/main/java/ServerLogic/actions/RequestPlayerInit.java
@@ -1,6 +1,6 @@
 package ServerLogic.actions;
 
-import static ClientUIHandling.Constants.GameViewActivityType;
+import static ClientUIHandling.Constants.GAMEVIEW_ACTIVITY_TYPE;
 import static ClientUIHandling.Constants.PREFIX_INIT_PLAYERS;
 
 import ServerLogic.ServerActionInterface;
@@ -9,6 +9,6 @@ import network2.ServerNetworkClient;
 public class RequestPlayerInit implements ServerActionInterface {
     @Override
     public void execute(ServerNetworkClient server, Object parameters) {
-        server.broadcast(GameViewActivityType, PREFIX_INIT_PLAYERS, new String[]{(String) parameters});
+        server.broadcast(GAMEVIEW_ACTIVITY_TYPE, PREFIX_INIT_PLAYERS, new String[]{(String) parameters});
     }
 }
