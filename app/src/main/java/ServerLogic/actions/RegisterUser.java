77802b588adf0bf8b728c5b71ce2a8e01d8a4800
djamn <david@jamnig.net>
2023-05-06 21:15:49 +0200

    feat: reworked user Register server class

--- app/src/main/java/ServerLogic/actions/RegisterUser.java
+++ app/src/main/java/ServerLogic/actions/RegisterUser.java
@@ -2,6 +2,10 @@ package ServerLogic.actions;
 
 import android.util.Log;
 
+import ClientUIHandling.ClientHandler;
+import ClientUIHandling.Config;
+import ClientUIHandling.Constants;
+import ServerLogic.ServerActionHandler;
 import ServerLogic.ServerActionInterface;
 import delta.dkt.activities.GameViewActivity;
 import delta.dkt.logic.structure.Game;
@@ -13,12 +17,16 @@ public class RegisterUser implements ServerActionInterface {
     public void execute(ServerNetworkClient server, Object parameters) {
         Log.d("[Server] Register User", "Register User request received! Server: " + server + " parameters: " + parameters);
 
+        if(Game.getPlayers().size() > Config.MAX_CLIENTS) return; // Prevent registering > max size
+
         int clientId = Game.getPlayers().size() + 1; // Starts at id 1 instead of 0
+        String nickname = "Player" + clientId; // Todo change to lobby names in next sprint
         String uuid = (String) parameters;
-        Log.d("[Server] Register User", "Debug - ClientID: " + clientId);
 
-        Game.getPlayers().put(clientId, new Player(uuid));
+        Game.getPlayers().put(clientId, new Player(nickname));
 
-        GameViewActivity.clientID = clientId; // Set gameview client ID
+        server.broadcast(Constants.LOBBYVIEW_ACTIVITY_TYPE, Constants.PREFIX_REGISTER, new String[]{uuid, String.valueOf(clientId)});
+
+//        GameViewActivity.clientID = clientId; // Set gameview client ID
     }
 }
