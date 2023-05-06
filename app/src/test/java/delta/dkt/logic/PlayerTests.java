2676dcb01267d7acd19d839ac4a2e58bf00d2707
Ozarks <45905689+Marty-Byrde@users.noreply.github.com>
2023-05-06 08:55:46 +0200

    fix: updated tests related to startCash
    Also applied the code styles to the test-cases.

--- app/src/test/java/delta/dkt/logic/PlayerTests.java
+++ app/src/test/java/delta/dkt/logic/PlayerTests.java
@@ -1,5 +1,6 @@
 package delta.dkt.logic;
 
+import ClientUIHandling.Config;
 import delta.dkt.logic.structure.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -20,7 +21,7 @@ class PlayerTests {
     Property testProperty2 = null;
 
     @BeforeEach
-    void setup () {
+    void setup() {
         testProperty1 = new Property(12, 10, 10, PropertyLevel.NORMAL, 10, 10);
         testProperty2 = new Property(14, 10, 10, PropertyLevel.NORMAL, 10, 10);
 
@@ -35,23 +36,15 @@ class PlayerTests {
      * Checks whether the initialized nickname, via the constructor, has been set and is accesible.
      */
     @Test
-    void checkNickname () {
+    void checkNickname() {
         assertEquals("Mike", player.getNickname());
-    }
-
-    /**
-     * Checks whether the player actually starts with the default cash.
-     */
-    @Test
-    void checkCash () {
-        assertEquals(Player.getStartCash(), player.getCash());
     }
 
     /**
      * Checks whether the default position of a player is set correclty and accessible.
      */
     @Test
-    void checkDefaultPosition () {
+    void checkDefaultPosition() {
         assertEquals(testProperty1, player.getPosition());
     }
 
@@ -59,25 +52,16 @@ class PlayerTests {
      * Check whether a player is not suspened by default.
      */
     @Test
-    void checkDefaultSuspension () {
+    void checkDefaultSuspension() {
         assertEquals(0, player.getSuspention());
     }
 
 
     /**
-     * Checks whether the static variable startCash can be accessed and modified
-     */
-    @Test
-    void checkStartCash_GetterSetter () {
-        Player.setStartCash(1000);
-        assertEquals(1000, Player.getStartCash());
-    }
-
-    /**
      * Checks whether the static variable playerCount can be accessed and modified
      */
     @Test
-    void checkGamePlayerCount_GetterSetter () {
+    void checkGamePlayerCount_GetterSetter() {
         Game.setPlayerCount(10);
         assertEquals(10, Game.getPlayerCount());
     }
@@ -86,7 +70,7 @@ class PlayerTests {
      * Checks whether the default nickname is set.
      */
     @Test
-    void checkDefaultPlayerNickname () {
+    void checkDefaultPlayerNickname() {
         player = new Player();
         assertTrue(player.getNickname().startsWith("Player-"));
     }
@@ -99,7 +83,7 @@ class PlayerTests {
      */
     @ParameterizedTest
     @ValueSource(booleans = {true, false})
-    void checkSuccessfulPropertyAcquisition (boolean onTop) {
+    void checkSuccessfulPropertyAcquisition(boolean onTop) {
         Property buying = onTop ? testProperty1 : testProperty2;
 
         if (onTop) {
@@ -116,13 +100,14 @@ class PlayerTests {
 
     /**
      * Checks whether the proceeding of a player attempting to buy a property when he does not have enough cash.
+     *
      * @param onTop parameterization for whether the player is on top of the property or not.
      */
     @ParameterizedTest
     @ValueSource(booleans = {true, false})
-    void checkPropertyAcqusition_LowCash (boolean onTop) {
-        Player.setStartCash(0);
+    void checkPropertyAcqusition_LowCash(boolean onTop) {
         player = new Player();
+        player.setCash(0);
 
         Property buying = onTop ? testProperty1 : testProperty2;
 
@@ -138,11 +123,12 @@ class PlayerTests {
 
     /**
      * Checks whether a property cannot be bought when it is already owned by someone else.
+     *
      * @param onTop parameterization for whether the player is on top of the property or not.
      */
     @ParameterizedTest
     @ValueSource(booleans = {true, false})
-    void checkPropertyAcqusition_Owned (boolean onTop) {
+    void checkPropertyAcqusition_Owned(boolean onTop) {
         Property buying = onTop ? testProperty1 : testProperty2;
         buying.setOwner(new Player("Maxi"));
 
@@ -158,11 +144,12 @@ class PlayerTests {
 
     /**
      * Checks whether the acquisition process handles the amtept to buy a field, thus no property, correclty.
+     *
      * @param onTop parameterization for whether the player is on top of the property or not.
      */
     @ParameterizedTest
     @ValueSource(booleans = {true, false})
-    void checkPropertyAcqusition_InvalidField (boolean onTop) {
+    void checkPropertyAcqusition_InvalidField(boolean onTop) {
         Game.setMap(mockMapHandling);
 
         Field invalidField = new SomeTestField(16);
@@ -188,11 +175,11 @@ class PlayerTests {
      * Checks whether a property owned by a player can be refuned successfully
      */
     @Test
-    void checkPropertyRefund_Successful () {
+    void checkPropertyRefund_Successful() {
         player.buyProperty();
 
         assertTrue(player.sellProperty(testProperty1.getLocation()));
-        assertEquals(Player.getStartCash() - testProperty1.getPrice() / 2, player.getCash());
+        assertEquals(Config.INITIAL_CASH - testProperty1.getPrice() / 2, player.getCash());
         assertEquals(0, player.getProperties().size());
         assertNull(testProperty1.getOwner());
         assertEquals(0, testProperty1.getAccessories().size());
@@ -202,7 +189,7 @@ class PlayerTests {
      * Checks whether a player can sell a property that he does not own.
      */
     @Test
-    void checkPropertyRefund_NotOwned () {
+    void checkPropertyRefund_NotOwned() {
         assertFalse(player.sellProperty(testProperty1.getLocation()));
 
         testProperty1.setOwner(new Player("Herbert"));
@@ -213,7 +200,7 @@ class PlayerTests {
      * Check whether the attempt of selling a field that is no property is handled correctly.
      */
     @Test
-    void checkPropertyRefund_InvalidField () {
+    void checkPropertyRefund_InvalidField() {
         Game.setMap(mockMapHandling);
 
         Field invalidField = new SomeTestField(16);
@@ -232,7 +219,7 @@ class PlayerTests {
      * Checks whether a player can be suspended for a given amount of rounds.
      */
     @Test
-    void checkSettingPlayerTimeout () {
+    void checkSettingPlayerTimeout() {
         player.setSuspension(10);
         assertEquals(10, player.getSuspention());
     }
@@ -241,7 +228,7 @@ class PlayerTests {
      * Checks whether the suspension state can be accessed.
      */
     @Test
-    void checkIsPlayerSuspended () {
+    void checkIsPlayerSuspended() {
         checkSettingPlayerTimeout();
         assertTrue(player.isSuspended());
     }
@@ -250,7 +237,7 @@ class PlayerTests {
      * Check whether the suspension of a player can be lifted.
      */
     @Test
-    void checkResetSuspension () {
+    void checkResetSuspension() {
         checkSettingPlayerTimeout();
         player.resetSuspension();
         assertFalse(player.isSuspended());
@@ -261,7 +248,7 @@ class PlayerTests {
      * Check whether reducing a player's suspension works properly.
      */
     @Test
-    void checkReducingSuspension () {
+    void checkReducingSuspension() {
         checkSettingPlayerTimeout();
 
         for (int i = 10; i > 0; i--) {
@@ -284,7 +271,7 @@ class PlayerTests {
     /**
      * This method will set up the mock-requirements for testing the property-payment-cycle
      */
-    void setMockRequirements_PropertyAquisition () {
+    void setMockRequirements_PropertyAquisition() {
         Game.setMap(mockMapHandling);
         when(mockMapHandling.getField(1)).thenReturn(testProperty1);
         when(mockMapHandling.getField(testProperty1.getLocation())).thenReturn(testProperty1);
@@ -296,7 +283,7 @@ class PlayerTests {
     /**
      * This method will verify the calls of the previously mocked methods.
      */
-    void verifyMockCalls (Field bought) {
+    void verifyMockCalls(Field bought) {
         verify(mockMapHandling, atLeastOnce()).getField(1);
         verify(mockMapHandling, atLeastOnce()).getField(bought.getLocation());
     }
@@ -306,10 +293,10 @@ class PlayerTests {
      * it will check if the owner property has been set correctly,
      * it will check whether the player cash has been modified correctly.
      */
-    void testSuccessfulPropertyAcquisition (int exProperties, int propertyPrice) {
+    void testSuccessfulPropertyAcquisition(int exProperties, int propertyPrice) {
         assertEquals(exProperties, player.getProperties().size());
         assertEquals(player, player.getProperties().get(0).getOwner());
-        assertEquals((Player.getStartCash() - propertyPrice), player.getCash());
+        assertEquals((Config.INITIAL_CASH - propertyPrice), player.getCash());
     }
 
 
@@ -317,17 +304,18 @@ class PlayerTests {
 
     /**
      * Checks whether moving the player with a given amount of steps is successful
+     *
      * @param steps Steps that the player will move from its current position.
      */
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
-    void checkPlayerMovement_bySteps (int steps) {
+    void checkPlayerMovement_bySteps(int steps) {
 
         //? sets the return value for getField method with its given arguments to a valid property.
         int location = player.getPosition().getLocation() + steps;
-        if(steps%2==0) {
+        if (steps % 2 == 0) {
             when(mockMapHandling.getField(location)).thenReturn(generateDummyProperty(location));
-        }else{
+        } else {
             //Test for movement on a special location
             when(mockMapHandling.getField(location)).thenReturn(new SpecialField(location));
         }
@@ -342,11 +330,12 @@ class PlayerTests {
 
     /**
      * Checks whether a players position can be set by moving the player to a given position.
+     *
      * @param location The location to which the player should be moved.
      */
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3, 4, 5, 6, 10})
-    void checkPlayerMovement_moveTo (int location) {
+    void checkPlayerMovement_moveTo(int location) {
 
         //? sets the return value for getField method with its given arguments to a valid property.
         when(mockMapHandling.getField(location)).thenReturn(generateDummyProperty(location));
@@ -362,7 +351,7 @@ class PlayerTests {
      * Checks whether a player is prohibited from moving when suspended.
      */
     @Test
-    void checkPlayerMovement_Suspended () {
+    void checkPlayerMovement_Suspended() {
         when(mockMapHandling.getFields()).thenReturn(generateDummyList());
 
         player.setSuspension(10);
@@ -381,7 +370,7 @@ class PlayerTests {
      * For now, this test will stay in this class since it tests all the code written in this branch.
      */
     @Test
-    void checkGameMap_getFields () {
+    void checkGameMap_getFields() {
         assertDoesNotThrow(() -> {
             assertEquals(40, new GameMap().getFields().size());
         });
@@ -392,38 +381,39 @@ class PlayerTests {
      * For now, this test will stay in this class since it tests all the code written in this branch.
      */
     @Test
-    void checkGameMap_getField () {
+    void checkGameMap_getField() {
         assertNotNull(new GameMap().getField(10));
     }
 
 
     @Test
-    void checkPlayer_payRent () {
+    void checkPlayer_payRent() {
 
         Player testPlayer = new Player();
         Player testOwner = new Player();
 
-        Property p = new Property(1,10,10,PropertyLevel.CHEAP,10,10);
+        Property p = new Property(1, 10, 10, PropertyLevel.CHEAP, 10, 10);
         p.setOwner(testOwner);
-        int amount =  p.calculateRent();
+        int amount = p.calculateRent();
         int cashBeforePlayer = testPlayer.getCash();
         int cashBeforeOwner = testOwner.getCash();
         testPlayer.payRentTo(testOwner, amount);
 
-        assertEquals(cashBeforePlayer-amount, testPlayer.getCash());
-        assertEquals(cashBeforeOwner+amount, testOwner.getCash());
+        assertEquals(cashBeforePlayer - amount, testPlayer.getCash());
+        assertEquals(cashBeforeOwner + amount, testOwner.getCash());
 
     }
 
     /**
      * This method will create a property object that is being used as a valid field.
+     *
      * @param location The location of the property-field.
      */
-    Field generateDummyProperty (int location) {
+    Field generateDummyProperty(int location) {
         return new Property(location, 0, 0, PropertyLevel.NORMAL, 0, 0);
     }
 
-    ArrayList<Field> generateDummyList () {
+    ArrayList<Field> generateDummyList() {
         ArrayList<Field> dummy = new ArrayList<>();
         for (int i = 0; i < 40; i++) dummy.add(null);
 
@@ -434,7 +424,7 @@ class PlayerTests {
 
 class SomeTestField extends Field {
 
-    public SomeTestField (int location) {
+    public SomeTestField(int location) {
         super(location);
     }
 }
