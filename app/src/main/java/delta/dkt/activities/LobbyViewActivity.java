b631b8cc84bae1592f880b4a94949a17bf5466c5
Matthaeus <m.gustab@hotmail.com>
2023-05-07 01:38:35 +0200

    feat: moved subscribeToLogic statement to the beginning of onCreate,

--- app/src/main/java/delta/dkt/activities/LobbyViewActivity.java
+++ app/src/main/java/delta/dkt/activities/LobbyViewActivity.java
@@ -4,8 +4,8 @@ import static ClientUIHandling.Constants.PREFIX_ADD_USER_TO_LIST;
 import static ClientUIHandling.Constants.PREFIX_GAME_START;
 import static ClientUIHandling.Constants.PREFIX_REMOVE_USER_FROM_LIST;
 import static ClientUIHandling.Constants.PREFIX_UPDATE_USER_LIST;
-import static delta.dkt.activities.MainActivity.INTENT_PARAMETER;
-import static delta.dkt.activities.MainMenuActivity.username;
+import static delta.dkt.activities.MainActivity.user;
+import static delta.dkt.activities.MainMenuActivity.role;
 
 
 import android.content.pm.ActivityInfo;
@@ -39,16 +39,18 @@ public class LobbyViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lobby_view);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT); // Force portrait screen at activity level
 
+        MainActivity.subscribeToLogic(Constants.LOBBYVIEW_ACTIVITY_TYPE, this);
+
         // Get Views from the Lobby xml:
         Button backButton = findViewById(R.id.backbtn);
         Button startButton = findViewById(R.id.startbtn);
-
+        //boolean role = getIntent().getExtras().getBoolean("role");
 
 
         // Everything which belongs to the Recycler View:
         recyclerView = findViewById(R.id.lobbyRecyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new UserNameAdapter(this, userList);
+        adapter = new UserNameAdapter(this, userList, role);
         recyclerView.setAdapter(adapter);
 
         // Adding User to the UserList
@@ -67,7 +69,6 @@ public class LobbyViewActivity extends AppCompatActivity {
             ServerActionHandler.triggerAction(PREFIX_GAME_START, "");
         });
 
-        MainActivity.subscribeToLogic(Constants.LOBBYVIEW_ACTIVITY_TYPE, this);
     }
 
 
@@ -79,13 +80,13 @@ public class LobbyViewActivity extends AppCompatActivity {
 
     // This Method removes the User from the UserList
     public void removeUserFromUserList() {
-        ServerActionHandler.triggerAction(PREFIX_REMOVE_USER_FROM_LIST, username);
+        ServerActionHandler.triggerAction(PREFIX_REMOVE_USER_FROM_LIST, user);
         ServerActionHandler.triggerAction(PREFIX_UPDATE_USER_LIST,null);
         Toast.makeText(LobbyViewActivity.this, "Users Total: "+userList.size(), Toast.LENGTH_SHORT).show();
     }
 
     public void addUserToUserListAndUpdate () {
-        ServerActionHandler.triggerAction(PREFIX_ADD_USER_TO_LIST, username);
+        ServerActionHandler.triggerAction(PREFIX_ADD_USER_TO_LIST, user);
         ServerActionHandler.triggerAction(PREFIX_UPDATE_USER_LIST, null);
         Toast.makeText(LobbyViewActivity.this, "Users Total: "+userList.size(), Toast.LENGTH_SHORT).show();
     }
