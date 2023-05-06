bf5e6ee34e124875f1f3570fc264632547d7a05c
Anja81 <anja.adamitsch@gmx.at>
2023-05-06 15:23:59 +0200

    feat: added a constructor and setManager method to the class DiscoveryListener

--- app/src/main/java/network2/DiscoveryListener.java
+++ app/src/main/java/network2/DiscoveryListener.java
@@ -3,7 +3,20 @@ package network2;
 import android.net.nsd.NsdManager;
 import android.net.nsd.NsdServiceInfo;
 
+import delta.dkt.activities.FindHostViewActivity;
+
 public class DiscoveryListener implements NsdManager.DiscoveryListener{
+    private FindHostViewActivity activity;
+    private NsdManager manager;
+
+    public DiscoveryListener(FindHostViewActivity activity){
+        this.activity = activity;
+    }
+
+    public void setManager(NsdManager manager){
+        this.manager = manager;
+    }
+
     @Override
     public void onStartDiscoveryFailed(String s, int i) {
 
