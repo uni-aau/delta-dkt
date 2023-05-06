cade77d0a8b288f641d98340708eaf61532b38ad
Anja81 <anja.adamitsch@gmx.at>
2023-05-06 15:19:29 +0200

    feat: added the class DiscoveryListener and changed the startDiscovery method in NetworkServiceDiscoveryClient

---
+++ app/src/main/java/network2/DiscoveryListener.java
@@ -0,0 +1,36 @@
+package network2;
+
+import android.net.nsd.NsdManager;
+import android.net.nsd.NsdServiceInfo;
+
+public class DiscoveryListener implements NsdManager.DiscoveryListener{
+    @Override
+    public void onStartDiscoveryFailed(String s, int i) {
+
+    }
+
+    @Override
+    public void onStopDiscoveryFailed(String s, int i) {
+
+    }
+
+    @Override
+    public void onDiscoveryStarted(String s) {
+
+    }
+
+    @Override
+    public void onDiscoveryStopped(String s) {
+
+    }
+
+    @Override
+    public void onServiceFound(NsdServiceInfo nsdServiceInfo) {
+
+    }
+
+    @Override
+    public void onServiceLost(NsdServiceInfo nsdServiceInfo) {
+
+    }
+}
