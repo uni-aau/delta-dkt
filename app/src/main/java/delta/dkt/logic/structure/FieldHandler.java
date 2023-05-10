9ac74a669abaaca9dcd826bd8f624ca7be01a5fc
Anja81 <anja.adamitsch@gmx.at>
2023-05-10 16:19:21 +0200

    feat: added PrisonField class

--- app/src/main/java/delta/dkt/logic/structure/FieldHandler.java
+++ app/src/main/java/delta/dkt/logic/structure/FieldHandler.java
@@ -18,7 +18,7 @@ public class FieldHandler {
             if (i == 1) fields.add(new SpecialField(i, "Start"));
             if (i == 11) fields.add(new SpecialField(i, "Gesetztes Verletzung"));
             if (i == 21) fields.add(new SpecialField(i, "Vermögens Abgabe"));
-            if (i == 31) fields.add(new SpecialField(i, "Gefängnis"));
+            if (i == 31) fields.add(new PrisonField(i, "Gefängnis"));
         }
 
         return fields;
