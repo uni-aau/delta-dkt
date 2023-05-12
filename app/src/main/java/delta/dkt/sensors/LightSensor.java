a4026cd5e6cb8c051f55a28582ef6745214deeb4
Ozarks <45905689+Marty-Byrde@users.noreply.github.com>
2023-05-12 16:31:42 +0200

    feat: simplified cover-state accessibility

--- app/src/main/java/delta/dkt/sensors/LightSensor.java
+++ app/src/main/java/delta/dkt/sensors/LightSensor.java
@@ -6,6 +6,11 @@ import android.hardware.SensorEventListener;
 
 public class LightSensor implements SensorEventListener {
     public static float value;
+    private static final float COVER_THRESHOLD = 300;
+
+    public boolean isCovered() {
+        return value > COVER_THRESHOLD;
+    }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
