5ff4e2d088c02a6471cdd211145bf31a8d5906d3
Ozarks <45905689+Marty-Byrde@users.noreply.github.com>
2023-05-15 11:22:28 +0200

    fix: inverted isCovered condition

--- app/src/main/java/delta/dkt/sensors/LightSensor.java
+++ app/src/main/java/delta/dkt/sensors/LightSensor.java
@@ -3,18 +3,20 @@ package delta.dkt.sensors;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.util.Log;
 
 public class LightSensor implements SensorEventListener {
     public static float value;
     private static final float COVER_THRESHOLD = 300;
 
     public static boolean isCovered() {
-        return value > COVER_THRESHOLD;
+        return value < COVER_THRESHOLD;
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         LightSensor.value = sensorEvent.values[0];
+        Log.d("Movement-LightSensor", "Value has changed to: "+sensorEvent.values[0]);
     }
 
     @Override
