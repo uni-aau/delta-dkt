56f17500436edaca0822696dbba98a23c2fc44f0
djamn <david@jamnig.net>
2023-05-15 21:34:38 +0200

    feat: implemented set start money action

---
+++ app/src/main/java/ClientUIHandling/actions/ActionSetStartMoney.java
@@ -0,0 +1,27 @@
+package ClientUIHandling.actions;
+
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import ClientUIHandling.ClientActionInterface;
+import ClientUIHandling.Constants;
+import delta.dkt.R;
+import delta.dkt.activities.GameViewActivity;
+
+public class ActionSetStartMoney implements ClientActionInterface {
+    @Override
+    public void execute(AppCompatActivity activity, String clientMessage) {
+        String[] args = clientMessage.replace(Constants.PREFIX_START_CASH_VALUE, "").trim().split(";");
+        int clientId = Integer.parseInt(args[0]);
+        int newCashValue = Integer.parseInt(args[1]);
+
+        Log.d("[Client] Start Money", "Client start money action received - ClientID = " + clientId);
+
+        if (GameViewActivity.clientID == clientId) {
+            ((TextView) activity.findViewById(R.id.textView_cash)).setText(String.format(activity.getString(R.string.cash_text), newCashValue));
+        }
+
+    }
+}
