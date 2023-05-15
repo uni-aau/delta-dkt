e52c181913c6f96d4880c18fd19935a65fc73dc5
djamn <david@jamnig.net>
2023-05-15 23:23:52 +0200

    fix: fixed typo & implemented error message

--- app/src/main/java/ClientUIHandling/actions/ActionActivityBroadcast.java
+++ app/src/main/java/ClientUIHandling/actions/ActionActivityBroadcast.java
@@ -1,5 +1,6 @@
 package ClientUIHandling.actions;
 
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,14 +16,16 @@ public class ActionActivityBroadcast implements ClientActionInterface {
         String[] args = clientMessage.replace(Constants.PREFIX_ACTIVITY_BROADCAST, "").trim().split(";"); // Holt sich Args nach dem PREFIX
         String messageTag = args[0]; // get message tag
         String playerName = "null";
-        String activityMessageValue = "&s - Unknown message tag";
+        String activityMessageValue = "%s - Unknown message tag";
         if (args[1] != null) {
             playerName = args[1];
         }
 
-        int activityMessageIdentifier = activity.getResources().getIdentifier(messageTag, "string", activity.getPackageName()); // get resourcekey from identifier
+        int activityMessageIdentifier = activity.getResources().getIdentifier(messageTag, "string", activity.getPackageName()); // get resource key from identifier
         if (activityMessageIdentifier != 0) {
-            activityMessageValue = activity.getResources().getString(activityMessageIdentifier); // get string from resourcekey
+            activityMessageValue = activity.getResources().getString(activityMessageIdentifier); // get string from resource key
+        } else {
+            Log.e("[CLIENT]", "Wrong message tag defined messageTag = " + messageTag);
         }
 
         String activityMessage = String.format(activityMessageValue, playerName); // sets playername
