53ae1abc6c824268806004adc451bcfa83b971b7
Ozarks <45905689+Marty-Byrde@users.noreply.github.com>
2023-05-18 10:33:54 +0200

    refactor: applied code styles

--- app/src/main/java/ClientUIHandling/handlers/languages/LanguageHandler.java
+++ app/src/main/java/ClientUIHandling/handlers/languages/LanguageHandler.java
@@ -8,39 +8,41 @@ import android.widget.TextView;
 import java.util.Arrays;
 
 public class LanguageHandler {
-    private LanguageHandler(){}
+    private LanguageHandler() {
+    }
 
-    public static String formatText(String template, Object[] args){
+    public static String formatText(String template, Object[] args) {
         int requestedArgs = 0;
-        if(template.contains("%s")) requestedArgs = template.split("%s").length - 1;
+        if (template.contains("%s")) requestedArgs = template.split("%s").length - 1;
 
         Object[] acceptedArgs = new String[requestedArgs];
         Arrays.fill(acceptedArgs, "(missing)");
 
         //* Fill requested args if provided.
-        if(args.length != 0){
-            for(int i = 0; i < acceptedArgs.length; i++){
-                if(args.length > i) acceptedArgs[i] = args[i];
+        if (args.length != 0) {
+            for (int i = 0; i < acceptedArgs.length; i++) {
+                if (args.length > i) acceptedArgs[i] = args[i];
                 else acceptedArgs[i] = "(missing)";
             }
         }
 
         //? Add arguments to the format, that exceed its placeholder count.
         StringBuilder exceeded = new StringBuilder();
-        for(int i = acceptedArgs.length; i < args.length; i++){
+        for (int i = acceptedArgs.length; i < args.length; i++) {
             exceeded.append(args[i]);
-            if(i != args.length -1) exceeded.append("|");
+            if (i != args.length - 1) exceeded.append("|");
         }
 
         return String.format(template, acceptedArgs) + String.format("(%s)", exceeded);
     }
     
+
     @SuppressLint("DiscouragedApi")
-    public static void updateTextElement(Activity activity, String fieldName, String strings, String[] args){
+    public static void updateTextElement(Activity activity, String fieldName, String strings, String[] args) {
         int textElementIdentifier = activity.getResources().getIdentifier(fieldName, "id", activity.getPackageName());
         TextView textElement = activity.findViewById(textElementIdentifier);
 
-        if(textElement == null) {
+        if (textElement == null) {
             Log.w("Game-Translation", String.format("Translation failed, as the view-element (%s) was not found!", fieldName));
             return;
         }
