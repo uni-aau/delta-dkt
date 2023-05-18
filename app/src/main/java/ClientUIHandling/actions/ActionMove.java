97250b2d0e0b5b8823760a8aad42a0c6e9dcf504
Ozarks <45905689+Marty-Byrde@users.noreply.github.com>
2023-05-18 11:13:59 +0200

    feat: tested the LanguageHandler
    The ui elements text is updated using a template string in which the arguments are inserted into.

--- app/src/main/java/ClientUIHandling/actions/ActionMove.java
+++ app/src/main/java/ClientUIHandling/actions/ActionMove.java
@@ -1,5 +1,6 @@
 package ClientUIHandling.actions;
 
+import ClientUIHandling.handlers.languages.LanguageHandler;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -45,6 +46,9 @@ public class ActionMove implements ClientActionInterface {
 
         var snack = SnackBarHandler.createSnackbar(activity.findViewById(R.id.imageView), result, 2000, true, "#6481d5", null);
         snack.show();
+
+        Log.d("Game-Translation", "Updating Ui element with new text!");
+        LanguageHandler.updateTextElement(activity, "textView_cash", "cash_text", new String[]{"First", "Second", "Third", "Fourth", "Fifth", "Sixth"});
 
         /*if(MainMenuActivity.role) {
             ServerActionHandler.triggerAction(Constants.PREFIX_ROLL_DICE_REQUEST, clientID); // Sets the next player to roll the dice
