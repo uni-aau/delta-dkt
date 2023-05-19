eedc8ca9dfe432c1053aeec49594d7944da641ce
djamn <david@jamnig.net>
2023-05-19 13:21:53 +0200

    feat: added new specialfield

--- app/src/main/java/delta/dkt/logic/structure/FieldHandler.java
+++ app/src/main/java/delta/dkt/logic/structure/FieldHandler.java
@@ -18,6 +18,7 @@ public class FieldHandler {
             if (i == 11) fields.add(new SpecialField(i, "Gesetztes Verletzung"));
             if (i == 21) fields.add(new SpecialField(i, "VermögensAbgabe"));
             if (i == 31) fields.add(new SpecialField(i, "Gefängnis"));
+            if (i == 33) fields.add(new SpecialField(i, "Steuerabgabe"));
         }
 
         return fields;
