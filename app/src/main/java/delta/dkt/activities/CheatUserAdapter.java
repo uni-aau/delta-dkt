7d6d108421f3492336d1700f79a569c54d9230ae
Ozarks <45905689+Marty-Byrde@users.noreply.github.com>
2023-05-24 10:04:35 +0200

    feat: removed unused imports - CheatAdapter

--- app/src/main/java/delta/dkt/activities/CheatUserAdapter.java
+++ app/src/main/java/delta/dkt/activities/CheatUserAdapter.java
@@ -1,8 +1,5 @@
 package delta.dkt.activities;
 
-import android.content.Context;
-import android.graphics.Color;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
