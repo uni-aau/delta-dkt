4b237af97e326f7d37530ad4e996f46e6f4640a3
djamn <david@jamnig.net>
2023-05-27 16:01:13 +0200

    fix: renamed subSegments to propertyValues for better understanding

--- app/src/main/java/ClientUIHandling/actions/ActionPropertyListUpdate.java
+++ app/src/main/java/ClientUIHandling/actions/ActionPropertyListUpdate.java
@@ -17,22 +17,20 @@ public class ActionPropertyListUpdate implements ClientActionInterface {
     public void execute(AppCompatActivity activity, String clientMessage) {
         String[] args = clientMessage.replace(PREFIX_PROPLIST_UPDATE, "").trim().split(";");
         ArrayList<PropertyListElement> propertyElements = new ArrayList<>();
-//        args.add(String.format("%d#%s#%d#%d#%d", i, owner, property.getPrice(), property.getBaseRent(), property.getHouses()));
 
         for (String arg : args) {
-            String[] subSegments = arg.split("#");
-            int propNumber = Integer.parseInt(subSegments[0]);
-            Property property = (Property) Game.getMap().getField(propNumber);
+            String[] propertyValues = arg.split("#"); // Splits String into all variables
 
-            String propertyNumber = subSegments[0];
-            String propertyPrice = subSegments[1];
-            String propertyBaseRent = subSegments[2];
-            String propertyOwner = subSegments[3];
-            int propertyHouses = Integer.parseInt(subSegments[4]);
+            String propertyNumber = propertyValues[0];
+            String propertyPrice = propertyValues[1];
+            String propertyBaseRent = propertyValues[2];
+            String propertyOwner = propertyValues[3];
+            int propertyHouses = Integer.parseInt(propertyValues[4]);
+            Property property = (Property) Game.getMap().getField(Integer.parseInt(propertyNumber));
 
             PropertyListElement element = new PropertyListElement(propertyNumber, property.getName(), propertyPrice, propertyBaseRent, propertyOwner, propertyHouses);
             propertyElements.add(element);
         }
-        PropertyListActivity.messages = propertyElements;
+        PropertyListActivity.propertyElements = propertyElements;
     }
 }
