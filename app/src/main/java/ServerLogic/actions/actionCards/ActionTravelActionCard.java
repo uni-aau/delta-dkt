55597af4e9280167482ec5877122a71cd8093024
Wolfgang Prett <wolfi-92@gmx.net>
2023-05-29 09:20:10 +0200

    added travel card server action
    that sends info to clients when a player is affected by TravelActionCards.
    For now , the client's only reaction is to reflect the affected players new position on the gameboard.

---
+++ app/src/main/java/ServerLogic/actions/actionCards/ActionTravelActionCard.java
@@ -0,0 +1,36 @@
+package ServerLogic.actions.actionCards;
+
+import static ClientUIHandling.Constants.GAMEVIEW_ACTIVITY_TYPE;
+import static ClientUIHandling.Constants.PREFIX_PLAYER_MOVE;
+
+import android.util.Log;
+
+
+import ServerLogic.ServerActionInterface;
+import delta.dkt.logic.structure.Game;
+import network2.ServerNetworkClient;
+
+public class ActionTravelActionCard implements ServerActionInterface {
+    @Override
+    public void execute(ServerNetworkClient server, Object parameters) {
+        try {
+            Object[] values = (Object[]) parameters;
+
+
+            int playerId = (int)values[0];
+            //int orignalStartPosition = (int)values[1];
+            int dest = Game.getPlayerById(playerId).getPosition().getLocation();
+
+            String[] args = new String[2];
+            args[0] = String.valueOf(Game.getClientIdByPlayerId(playerId));
+            args[1] = String.valueOf(dest);
+            //server.broadcast(PREFIX_ACTIONCARD_TRAVEL+" "+args[0]+" "+args[1]);
+            server.broadcast(GAMEVIEW_ACTIVITY_TYPE, PREFIX_PLAYER_MOVE, args);
+
+        }catch(Exception ex){
+            Log.d("Error" , ex.getMessage());
+            ex.printStackTrace();
+        }
+
+    }
+}
