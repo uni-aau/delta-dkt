ad9f52b94d90c53be7b0218796b76ae6ea1c0428
Wolfgang Prett <wolfi-92@gmx.net>
2023-05-29 10:07:13 +0200

    RiskTaskField: Changed property type to match baseclass

--- app/src/main/java/delta/dkt/logic/structure/RiskTaskField.java
+++ app/src/main/java/delta/dkt/logic/structure/RiskTaskField.java
@@ -5,14 +5,14 @@ package delta.dkt.logic.structure;
  */
 public class RiskTaskField extends Field{
 
-    RiskTask riskTask;
+    Task riskTask;
 
     /**
      * Creates a new risk task.
      * @param location location of the risk task field
      * @param riskTask the risk task that the player has to do
      */
-    public RiskTaskField(int location, RiskTask riskTask) {
+    public RiskTaskField(int location, Task riskTask) {
         super(location);
         this.riskTask = riskTask;
     }
