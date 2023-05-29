4c5adc0b15dd2b75e9b67afaa9672f9d3ebe7897
Wolfgang Prett <wolfi-92@gmx.net>
2023-05-29 09:34:49 +0200

    GoToJailActionCard: made defaultSuspensionRounds prop final
    adapted imports and changed server action prefix

--- app/src/main/java/delta/dkt/logic/structure/ActionCards/GoToJailActionCard.java
+++ app/src/main/java/delta/dkt/logic/structure/ActionCards/GoToJailActionCard.java
@@ -2,13 +2,12 @@ package delta.dkt.logic.structure.ActionCards;
 
 import ClientUIHandling.Constants;
 import ServerLogic.ServerActionHandler;
-import delta.dkt.logic.structure.Game;
 import delta.dkt.logic.structure.Player;
 import delta.dkt.logic.structure.Task;
 
 public class GoToJailActionCard extends Task {
 
-    private int defaultSuspensionRounds = 3;
+    private final static int defaultSuspensionRounds = 3;
 
     /**
      * Creates a new task.
@@ -23,10 +22,10 @@ public class GoToJailActionCard extends Task {
 
     @Override
     public void execute(Player asignee) {
-        asignee.suspendPlayerForRounds(this.defaultSuspensionRounds);
+        asignee.suspendPlayerForRounds(defaultSuspensionRounds);
 
         //send message to client that he is suspended for so many rounds
-        ServerActionHandler.triggerAction(Constants.PREFIX_PLAYER_MOVED_TO_PRISON,asignee.getId());
+        ServerActionHandler.triggerAction(Constants.PREFIX_ACTIONCARD_PRISON,asignee.getId());
     }
 
 }
