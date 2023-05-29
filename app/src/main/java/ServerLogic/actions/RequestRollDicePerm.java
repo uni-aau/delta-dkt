63e088f2e542e5b06d9719c77128592be2ed5c26
Wolfgang Prett <wolfi-92@gmx.net>
2023-05-29 10:06:21 +0200

    Adapted RequestRollDicePerm.java
    Changed next player determination and extended it so that it also checks if a player is imprisoned. made the next player determination continous, so that if the next player is imprisoned , either tries to free him (via OOJ-Card) or continues with the next possible.

--- app/src/main/java/ServerLogic/actions/RequestRollDicePerm.java
+++ app/src/main/java/ServerLogic/actions/RequestRollDicePerm.java
@@ -6,8 +6,6 @@ import static ClientUIHandling.Constants.PREFIX_ROLL_DICE_REQUEST;
 
 import android.util.Log;
 
-import ClientUIHandling.Config;
-import ClientUIHandling.Constants;
 import ServerLogic.ServerActionHandler;
 import ServerLogic.ServerActionInterface;
 import delta.dkt.logic.structure.Game;
@@ -15,46 +13,56 @@ import delta.dkt.logic.structure.Player;
 import network2.ServerNetworkClient;
 
 public class RequestRollDicePerm implements ServerActionInterface {
+
+
     @Override
     public void execute(ServerNetworkClient server, Object parameters) {
-        int nextClient;
-        String tag = "[Server] Roll Dice Request";
+        int nextClientId;
         int oldClientId = (int) parameters;
+        String tag = "[Server] Roll Dice Request";
         Log.d(tag, "Received next client request - Old client: " + parameters);
 
-        // Check if clientID is last ID in game
+
+        //TODO: ADD ACTION HERE THAT EITHER SKIPS PLAYER IF IMPRISONED (decreases its turnCounter); USES GOOJ-Card or let him roll
+        int nextCopy = oldClientId;
+        Player p;
+        do {
+            nextClientId = getNextClientId(nextCopy);
+            p = getNextEligiblePlayer(nextClientId);
+            nextCopy = nextClientId;
+        }
+        while (p == null);
+
+        Log.d("[SERVER]", "New roll dice server request - prevClientID " + oldClientId + " nextClient " + nextClientId + " nickName " + p.getNickname());
+
+        server.broadcast(GAMEVIEW_ACTIVITY_TYPE, PREFIX_ROLL_DICE_REQUEST, new String[]{String.valueOf(nextClientId), p.getNickname()});
+        ServerActionHandler.triggerAction(PREFIX_PLAYER_MOVE, oldClientId);
+
+        Game.incrementRounds(oldClientId);
+    }
+
+
+
+    //TODO: IF WE WANT CLIENTS TO DISPLAY SOMETHING WHEN A GOOJ-CARD IS USED THEN WE NEED TO TRIGGER AN ACTION HERE
+    private Player getNextEligiblePlayer(int nextClientId){
+        Player p1 = Game.getPlayers().get(nextClientId);
+        if(p1 ==null ) return null; //player not found returns null
+
+        if(p1.isSuspended()){
+            //TODO: if player is skipped, trigger an event to inform clients or display a visual
+            p1.handlePrisonRound();
+        }
+        return   p1.isSuspended() ? null : p1; //player could not be freed, return null
+    }
+
+    private int getNextClientId(int currentClientId){
         int size = Game.getPlayers().size();
-        nextClient = 0;
         if (size != 0) {
-            nextClient = (oldClientId % size) + 1;
-
-            String nickName = Game.getPlayers().get(nextClient).getNickname();
-            Log.d("[SERVER]", "New roll dice server request - prevClientID " + oldClientId + " nextClient " + nextClient + " nickName " + nickName);
-
-            Log.d(tag, "OldClientId = " + oldClientId + " NewClient = " + nextClient);
-            server.broadcast(GAMEVIEW_ACTIVITY_TYPE, PREFIX_ROLL_DICE_REQUEST, new String[]{String.valueOf(nextClient), nickName});
-            ServerActionHandler.triggerAction(PREFIX_PLAYER_MOVE, oldClientId);
-
-            Game.incrementRounds(oldClientId);
-
-        } else {
-            Log.e(tag, "Error - No players available in GameView");
+            return (currentClientId % size) + 1;
         }
+        return -1;
     }
 
-
-
-    /**
-     * @param nextPlayer: the player who´s turn it is going to be next
-     * @return a boolean if the player was freed from prison via card
-     */
-    private boolean handlePrisonCase(Player nextPlayer){
-    if(nextPlayer.isSuspended()){
-            //check if the player has a get-out-of-prison-card
-           // if(nextPlayer.)
-        }
-    return true;
-    }
-
+  
 
 }
