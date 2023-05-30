46a5f203f627d5f08aac85f10f4b808e6670297c
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-05-30 15:36:46 +0200

    test: tested hasValue for Double values

--- app/src/test/java/handlers/ParameterHandlerTests.java
+++ app/src/test/java/handlers/ParameterHandlerTests.java
@@ -33,4 +33,25 @@ class ParameterHandlerTests {
         Assertions.assertFalse(ParameterHandler.hasValue(array, 11, Integer.class));;
     }
 
+    /**
+     * Checks whether the values of the dummy array are handled correclty.
+     */
+    @Test
+    void checkDoubles(){
+        Assertions.assertTrue(ParameterHandler.hasValue(array, 0, Double.class)); // 0
+        Assertions.assertTrue(ParameterHandler.hasValue(array, 1, Double.class)); // 1
+        Assertions.assertTrue(ParameterHandler.hasValue(array, 2, Double.class)); // "0"
+        Assertions.assertTrue(ParameterHandler.hasValue(array, 3, Double.class)); // "1"
+        Assertions.assertFalse(ParameterHandler.hasValue(array, 4, Double.class)); // true
+        Assertions.assertFalse(ParameterHandler.hasValue(array, 5, Double.class)); // false
+        Assertions.assertFalse(ParameterHandler.hasValue(array, 6, Double.class)); // "true"
+        Assertions.assertFalse(ParameterHandler.hasValue(array, 7, Double.class)); // "false"
+        Assertions.assertTrue(ParameterHandler.hasValue(array, 8, Double.class)); // 10
+        Assertions.assertTrue(ParameterHandler.hasValue(array, 9, Double.class)); // 10.0
+        Assertions.assertTrue(ParameterHandler.hasValue(array, 10, Double.class)); // 10f
+        Assertions.assertFalse(ParameterHandler.hasValue(array, 11, Double.class)); // string
+        Assertions.assertFalse(ParameterHandler.hasValue(array, 12, Double.class)); // nullpointer
+    }
+
+
 }
