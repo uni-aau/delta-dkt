32820b3310bce7a2124b9205c7cce54c5d81391d
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-05-30 15:36:02 +0200

    test: tested hasValue for Integer values

---
+++ app/src/test/java/handlers/ParameterHandlerTests.java
@@ -0,0 +1,36 @@
+package handlers;
+
+import ServerLogic.handlers.ParameterHandler;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ParameterHandlerTests {
+
+    private Object[] array;
+
+    @BeforeEach
+    void setup(){
+        array = new Object[]{0, 1, "0", "1", true, false, "true", "false", 10, 10.0, 10f, "string"};
+    }
+
+    /**
+     * Checks whether the values of the dummy array are handled correctly.
+     */
+    @Test
+    void checkIntegers(){
+        Assertions.assertTrue(ParameterHandler.hasValue(array, 0, Integer.class));
+        Assertions.assertTrue(ParameterHandler.hasValue(array, 1, Integer.class));
+        Assertions.assertTrue(ParameterHandler.hasValue(array, 2, Integer.class));;
+        Assertions.assertTrue(ParameterHandler.hasValue(array, 3, Integer.class));;
+        Assertions.assertFalse(ParameterHandler.hasValue(array, 4, Integer.class));;
+        Assertions.assertFalse(ParameterHandler.hasValue(array, 5, Integer.class));;
+        Assertions.assertFalse(ParameterHandler.hasValue(array, 6, Integer.class));;
+        Assertions.assertFalse(ParameterHandler.hasValue(array, 7, Integer.class));;
+        Assertions.assertTrue(ParameterHandler.hasValue(array, 8, Integer.class));
+        Assertions.assertFalse(ParameterHandler.hasValue(array, 9, Integer.class));;
+        Assertions.assertFalse(ParameterHandler.hasValue(array, 10, Integer.class));;
+        Assertions.assertFalse(ParameterHandler.hasValue(array, 11, Integer.class));;
+    }
+
+}
