5e92c9efa30984a5e0fabbb93ef3a5b938970dd0
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-06-03 15:06:26 +0200

    feat: added prefix for client-request-redirect action
    Added a new prefix for an client-side action that will redirect a given request to the server, by the client that is running the server in the background.

--- app/src/main/java/ClientUIHandling/Constants.java
+++ app/src/main/java/ClientUIHandling/Constants.java
@@ -57,6 +57,7 @@ public class Constants {
   
     public static final String PREFIX_PLAYER_CHEAT_MENU = "CHEAT_MENU";
     public static final String PREFIX_PLAYER_REPORT_CHEATER = "REPORT_CHEATER";
+    public static final String PREFIX_REQUEST_SERVER_ACTION_AS_CLIENT = "REQUEST_SERVER_ACTION";
 
     public static final String LOG_MAIN = "Main";
     public static final String LOG_CHEAT = String.format("%s-Cheat", LOG_MAIN);
