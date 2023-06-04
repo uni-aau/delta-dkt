5a71bf33a4c2e6c1eb501e3c08b1c4feb49c960c
djamn <david@jamnig.net>
2023-06-04 13:43:06 +0200

    fix: restructured ActionPLayerInit created new initDice method for better visibility

--- app/src/main/java/ClientUIHandling/actions/ActionPlayerInit.java
+++ app/src/main/java/ClientUIHandling/actions/ActionPlayerInit.java
@@ -14,27 +14,23 @@ import ClientUIHandling.ClientActionInterface;
 import ClientUIHandling.Config;
 import delta.dkt.R;
 import delta.dkt.activities.GameViewActivity;
+import delta.dkt.activities.MainActivity;
+import delta.dkt.activities.MainMenuActivity;
 import delta.dkt.logic.structure.Game;
 
 public class ActionPlayerInit implements ClientActionInterface {
+    GameViewActivity gameViewActivity;
+    String userName;
+    String playerAmount;
     @Override
     public void execute(AppCompatActivity activity, String clientMessage) {
-        GameViewActivity gameViewActivity = (GameViewActivity) activity;
         String[] args = clientMessage.replace(PREFIX_INIT_PLAYERS, "").trim().split(";");
-        int clientId = Integer.parseInt(args[0]);
-        String userName = args[1];
+        gameViewActivity = (GameViewActivity) activity;
+        userName = args[0];
+        playerAmount = args[1];
 
-        Log.d("[UI] Roll Dice Perm", "Successfully received roll dice perm action from server handler: Activity: " + activity + " ClientID: " + clientId + " Start-Username: " + userName);
-
-        // Initial State (only client ID 1 can roll the dice)
-        ((TextView) activity.findViewById(R.id.textView_dice_information)).setText(String.format(activity.getString(R.string.dice_information_text), userName));
-        if (GameViewActivity.clientID == 1) {
-            gameViewActivity.enableDice();
-        } else {
-            gameViewActivity.disableDice();
-        }
-
-        setInitTextViewValues(activity, userName);
+        initDice();
+        setInitTextViewValues(activity, userName); // TODO
     }
 
     // Shows for every clientId a specific player marker in the gui (todo)
@@ -52,13 +48,25 @@ public class ActionPlayerInit implements ClientActionInterface {
         }
     }
 
+    private void initDice() {
+        Log.d("[UI] Roll Dice Perm", "Successfully received roll dice perm action from server handler: Activity: " + gameViewActivity + " ClientID: " + 1 + " Start-Username: " + userName);
+
+        // Initial State (only client ID 1 can roll the dice)
+        ((TextView) gameViewActivity.findViewById(R.id.textView_dice_information)).setText(String.format(gameViewActivity.getString(R.string.dice_information_text), userName));
+        if (GameViewActivity.clientID == 1) {
+            gameViewActivity.enableDice();
+        } else {
+            gameViewActivity.disableDice();
+        }
+    }
+
     // Sets initial textview values in GameView
     private void setInitTextViewValues(AppCompatActivity activity, String userName) {
         ((TextView) activity.findViewById(R.id.textView_cash)).setText(String.format(activity.getString(R.string.cash_text), String.valueOf(Config.INITIAL_CASH)));
         ((TextView) activity.findViewById(R.id.textView_my_properties)).setText(String.format(activity.getString(R.string.my_properties_text), String.valueOf(0)));
         ((TextView) activity.findViewById(R.id.textView_activity)).setText(String.format(activity.getString(R.string.activity_text), activity.getString(R.string.game_started_acitivty_text)));
 
-        String playerNameInputValue = String.format(activity.getString(R.string.playerName_info_text), userName);
+        String playerNameInputValue = String.format(activity.getString(R.string.playerName_info_text), MainMenuActivity.username);
         ((TextView) activity.findViewById(R.id.textView_playerName_spec)).setText(playerNameInputValue);
     }
 }
