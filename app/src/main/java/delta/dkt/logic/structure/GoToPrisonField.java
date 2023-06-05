ad1358d4f8c8a86bf64fcd9ff0de18cc3f0708ea
Anja81 <anja.adamitsch@gmx.at>
2023-06-05 22:03:09 +0200

    docs: added java-doc to GoToPrisonField class.

--- app/src/main/java/delta/dkt/logic/structure/GoToPrisonField.java
+++ app/src/main/java/delta/dkt/logic/structure/GoToPrisonField.java
@@ -1,5 +1,7 @@
 package delta.dkt.logic.structure;
-
+/**
+ * This class represents the field that sends a player to prison.
+ */
 public class GoToPrisonField extends SpecialField{
     public GoToPrisonField(int location, String name) {
         super(location, name);
