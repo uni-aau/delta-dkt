f6d2d723f2a613c143c3e7f28ba6f358245312ce
Ozarks <45905689+Marty-Byrde@users.noreply.github.com>
2023-06-07 13:27:03 +0200

    fix: resolved HashhMap primitive type code-smell
    Refactored the type of the clientID from int to Integer, so that its type matches the type of the HashMap.

--- app/src/main/java/ServerLogic/actions/ActionGoToPrison.java
+++ app/src/main/java/ServerLogic/actions/ActionGoToPrison.java
@@ -17,7 +17,7 @@ import network2.ServerNetworkClient;
 public class ActionGoToPrison implements ServerActionInterface {
     @Override
     public void execute(ServerNetworkClient server, Object parameters) {
-            int id = (int) parameters;
+            Integer id = (Integer) parameters;
             Player player = Game.getPlayers().get(id);
 
             int fieldLocation = player.getPosition().getLocation();
