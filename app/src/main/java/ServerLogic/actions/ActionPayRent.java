c6f9e324e2e08f91739ed66bb308df39678619c7
djamn <david@jamnig.net>
2023-06-09 02:10:04 +0200

    feat: added boolean parameter to playerLost trigger in ActionPayRent To evaluate player leave/lost event

--- app/src/main/java/ServerLogic/actions/ActionPayRent.java
+++ app/src/main/java/ServerLogic/actions/ActionPayRent.java
@@ -35,7 +35,7 @@ public class ActionPayRent implements ServerActionInterface {
                 if(owner != null && !property.getOwner().equals(player)){
                     int rent = property.calculateRent();
                     if(player.getCash() < rent){
-                        ServerActionHandler.triggerAction(PREFIX_PLAYER_LOST, player.getId());
+                        ServerActionHandler.triggerAction(PREFIX_PLAYER_LOST, new String[]{String.valueOf(player.getId()), "false"});
 
                     }else{
                         player.payRentTo(owner, rent);
