a135bce5c3f36acf45f8dac121385d8d2c9dff57
Matthaeus <m.gustab@hotmail.com>
2023-06-09 17:20:57 +0200

    feat: added Actions for users leaving the lobby

--- app/src/main/java/ClientUIHandling/actions/ActionLeaveLobby.java
+++ app/src/main/java/ClientUIHandling/actions/ActionLeaveLobby.java
@@ -1,18 +1,45 @@
 package ClientUIHandling.actions;
 
+
+import static ClientUIHandling.Constants.PREFIX_LEAVE_LOBBY;
+
+
+import android.content.Intent;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import ClientUIHandling.ClientActionInterface;
+import ClientUIHandling.ClientHandler;
+import delta.dkt.activities.LobbyViewActivity;
 import delta.dkt.activities.MainMenuActivity;
 
 public class ActionLeaveLobby implements ClientActionInterface {
     @Override
     public void execute(AppCompatActivity activity, String clientMessage) {
-        MainMenuActivity mainMenuActivity = new MainMenuActivity();
-        //mainMenuActivity.closeClient();
-        Log.d("[CLIENT]:Close_Game ", "Client closed");
+
+        String[] args = clientMessage.replace(PREFIX_LEAVE_LOBBY, "").trim().split(";");
+        String leavingUser = args[0];
+
+        Log.d("[CLIENT]:Leave_Lobby", leavingUser+" left!");
+
+        // Update the user card background in the LobbyViewActivity
+        LobbyViewActivity lobbyActivity = (LobbyViewActivity) activity;
+        lobbyActivity.runOnUiThread(() -> lobbyActivity.updateUserBackground(leavingUser));
+
+
+
+        // Close client connection
+        try {
+            ClientHandler.getClient().stopConnection();
+            Log.d("[CLIENT]:Client_closed", "");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+        Intent intent = new Intent(activity.getApplicationContext(), MainMenuActivity.class);
+        activity.startActivity(intent);
 
 
 
--- app/src/main/java/ServerLogic/actions/RequestLeaveLobby.java
+++ app/src/main/java/ServerLogic/actions/RequestLeaveLobby.java
@@ -3,15 +3,40 @@ package ServerLogic.actions;
 import static ClientUIHandling.Constants.LOBBYVIEW_ACTIVITY_TYPE;
 import static ClientUIHandling.Constants.PREFIX_LEAVE_LOBBY;
 
+
 import android.util.Log;
 
+import java.io.IOException;
+
 import ServerLogic.ServerActionInterface;
+import delta.dkt.activities.MainMenuActivity;
+import delta.dkt.logic.structure.Game;
 import network2.ServerNetworkClient;
 
 public class RequestLeaveLobby implements ServerActionInterface {
     @Override
     public void execute(ServerNetworkClient server, Object parameters) {
-        Log.d("[SERVER]:CLOSE_Game", "Close Game Request received");
-        server.broadcast(LOBBYVIEW_ACTIVITY_TYPE, PREFIX_LEAVE_LOBBY, new String[]{(String) parameters});
+
+        String leavingUser = (String) parameters;
+        Log.d("[SERVER]:Leave_Lobby", leavingUser+" is leaving the lobby!");
+        server.broadcast(LOBBYVIEW_ACTIVITY_TYPE, PREFIX_LEAVE_LOBBY, new String[]{(String) leavingUser});
+
+        // server.broadcast updateuserlist
+
+        if (MainMenuActivity.role) {
+            try {
+                //Wait for the message to be sent, then close the server
+                Thread.sleep(100);
+                server.tearDown();
+                Log.d("[SERVER]_Server_closed", "");
+                Game.getPlayers().clear();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (InterruptedException e) {
+                Log.w("Warning", "Interrupted!", e);
+                // Restore interrupted state...
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 }
