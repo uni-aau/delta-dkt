b15bf4909c916334cde3216fab896a13e22bc083
djamn <david@jamnig.net>
2023-06-11 23:08:30 +0200

    fix: added some comments to Config values for better understanding

--- app/src/main/java/ClientUIHandling/Config.java
+++ app/src/main/java/ClientUIHandling/Config.java
@@ -22,17 +22,17 @@ public class Config {
     public static boolean Skip = false; //? Used for quick development, skips activities until false.
     public static final boolean DEBUG = false;
 
-    public static final int START_CASH = 1000; //Number of rounds until the game ends
+    public static final int START_CASH = 1000;
     public static final boolean ONLY_SHOW_PROPERTY_WITH_OWNER = false;
     public static final int MAX_HOUSES = 4; // also used in displaying house amount (currently max. 4)
-    public static final int PUNISHMENT_FOR_CHEATING = 500;
-    public static final int PUNISHMENT_FOR_WRONG_REPORT = 200;
+    public static final int PUNISHMENT_FOR_CHEATING = 500; // cash for successful report
+    public static final int PUNISHMENT_FOR_WRONG_REPORT = 200; // cash deduction for unsuccessful report
 
     public static final Dice DICE_RANGE = new Dice(1, 6);
     public static final Dice CHEAT_RANGE = new Dice(7, 9);
-    public static final int TIMEOUT = 20000;
+    public static final int TIMEOUT = 20000; // timeout threshold (in milliseconds)
 
-    public static final int TIMEOUT_WARNING_THRESHOLD = 5000;
+    public static final int TIMEOUT_WARNING_THRESHOLD = 5000; // opens snackbar as warning that time is nearly over (in milliseconds)
 
     public static final int PING_TIMEOUT = 2000;
 }
