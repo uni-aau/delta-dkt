98b5d169323e6144817642a35c3a56e2a9474b4b
djamn <david@jamnig.net>
2023-06-11 21:43:02 +0200

    feat: Handled position in RequestAskBuyProperty

--- app/src/main/java/ServerLogic/actions/RequestAskBuyProperty.java
+++ app/src/main/java/ServerLogic/actions/RequestAskBuyProperty.java
@@ -9,10 +9,13 @@ import network2.ServerNetworkClient;
 public class RequestAskBuyProperty implements ServerActionInterface {
     @Override
     public void execute(ServerNetworkClient server, Object parameters) {
-        int clientID = (int) parameters;
+        String[] args = (String[]) parameters;
+
+        int clientID = Integer.parseInt(args[0]);
+        int position = Integer.parseInt(args[1]);
 
         Log.d("[Server] RequestAskBuyProperty", "Received property popup request! ClientID = " + clientID);
 
-        server.broadcast(Constants.GAMEVIEW_ACTIVITY_TYPE, Constants.PREFIX_ASK_BUY_PROPERTY, new String[]{String.valueOf(clientID)});
+        server.broadcast(Constants.GAMEVIEW_ACTIVITY_TYPE, Constants.PREFIX_ASK_BUY_PROPERTY, new String[]{String.valueOf(clientID), String.valueOf(position)});
     }
 }
