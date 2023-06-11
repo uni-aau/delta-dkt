30099508084681e43b6ff0b89794d30d08aa34af
djamn <david@jamnig.net>
2023-06-11 18:34:53 +0200

    feat: added activity broadcast when paying rent

--- app/src/main/java/ServerLogic/actions/ActionPayRent.java
+++ app/src/main/java/ServerLogic/actions/ActionPayRent.java
@@ -1,6 +1,7 @@
 package ServerLogic.actions;
 
 import static ClientUIHandling.Constants.GAMEVIEW_ACTIVITY_TYPE;
+import static ClientUIHandling.Constants.PREFIX_ACTIVITY_BROADCAST;
 import static ClientUIHandling.Constants.PREFIX_PLAYER_LOST;
 import static ClientUIHandling.Constants.PREFIX_PLAYER_RENTPAID;
 
@@ -40,6 +41,7 @@ public class ActionPayRent implements ServerActionInterface {
                     }else{
                         player.payRentTo(owner, rent);
                         server.broadcast(GAMEVIEW_ACTIVITY_TYPE +":"+PREFIX_PLAYER_RENTPAID+" "+player.getNickname()+"(id= "+player.getId()+" ) "+player.getCash()+" "+owner.getNickname()+"(id= "+owner.getId()+" ) "+owner.getCash());
+                        server.broadcast(GAMEVIEW_ACTIVITY_TYPE, PREFIX_ACTIVITY_BROADCAST, new String[]{"player_pay_rent_activity_text", player.getNickname(), String.valueOf(rent), property.getName(), owner.getNickname()});
                     }
 
                 }
