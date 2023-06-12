b6cebf149c7a92dc698295433d11768e55ff5217
Wolfgang Prett <wolfi-92@gmx.net>
2023-06-12 11:05:56 +0200

    fixed imports in serveraction

--- app/src/main/java/ServerLogic/actions/ActionOutOfJailCardAwarded.java
+++ app/src/main/java/ServerLogic/actions/ActionOutOfJailCardAwarded.java
@@ -1,9 +1,6 @@
 package ServerLogic.actions;
 
 
-import static ClientUIHandling.Constants.GAMEVIEW_ACTIVITY_TYPE;
-import static ClientUIHandling.Constants.PREFIX_HAS_PRISONCARD;
-
 import ServerLogic.ServerActionInterface;
 import network2.ServerNetworkClient;
 
