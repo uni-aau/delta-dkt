d6def210837ee6fca5abdda5ad1b38318a4c846f
Wolfgang Prett <wolfi-92@gmx.net>
2023-06-12 10:30:06 +0200

    Adapted Banktask to comply with new superclass. added execute method skeleton

--- app/src/main/java/delta/dkt/logic/structure/RiskTask.java
+++ app/src/main/java/delta/dkt/logic/structure/RiskTask.java
@@ -10,7 +10,7 @@ public class RiskTask extends Task{
      * Creates a new risk task.
      * @param id The id of the risk task
      * @param name The name of the risk task
-     * @param riskTask Tells you what task you have to do
+     * @param descriptionString Tells you what task you have to do
      */
     public RiskTask(int id, String name, String riskTask, String descriptionString) {
         super(id, name, descriptionString);
