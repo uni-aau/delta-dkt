67c2266a72a2d51902f68df88aaaabab4f47e0d9
Wolfgang Prett <wolfi-92@gmx.net>
2023-06-12 11:59:12 +0200

    fixed imports

--- app/src/main/java/delta/dkt/logic/structure/ActionCards/OutOfJailCard.java
+++ app/src/main/java/delta/dkt/logic/structure/ActionCards/OutOfJailCard.java
@@ -3,9 +3,10 @@ package delta.dkt.logic.structure.ActionCards;
 import ClientUIHandling.Constants;
 import ServerLogic.ServerActionHandler;
 import delta.dkt.logic.structure.Player;
+import delta.dkt.logic.structure.RiskTask;
 import delta.dkt.logic.structure.Task;
 
-public class OutOfJailCard extends Task {
+public class OutOfJailCard extends RiskTask {
 
     /**
      * Creates a new task.
@@ -23,7 +24,6 @@ public class OutOfJailCard extends Task {
         asignee.setYouGetOutOfPrisonCard(true);
 
         //send message to client that he is suspended for so many rounds
-
         ServerActionHandler.triggerAction(Constants.PREFIX_PRISONCARD_AWARDED,asignee.getId());
     }
 
