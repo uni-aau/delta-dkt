ae57c046282b5faf05252eb8f26354bfbc0d00a0
djamn <david@jamnig.net>
2023-06-13 19:00:16 +0200

    feat: updated selected mode check in RequestGameStartTime class Replaced SELECTED_MODE with IS_TIME_MODE

--- app/src/main/java/ServerLogic/actions/RequestGameStartTime.java
+++ app/src/main/java/ServerLogic/actions/RequestGameStartTime.java
@@ -33,7 +33,7 @@ public class RequestGameStartTime implements ServerActionInterface {
     private void initializeTimer() {
         cancelPreviousTimer();
 
-        int tickInterval = Config.SELECTED_GAME_MODE ? 1000 : -1000; // decreses countdown when user selects time
+        int tickInterval = Config.IS_TIME_MODE ? -1000 : 1000; // decreases countdown when user selects time
 
         timer = new CountDownTimer(Config.END_TIME, 1000) {
             @Override
@@ -53,7 +53,7 @@ public class RequestGameStartTime implements ServerActionInterface {
 
             @Override
             public void onFinish() {
-                if (!Config.SELECTED_GAME_MODE) { // Only stop the game via time when host selected max time as game mode
+                if (Config.IS_TIME_MODE) { // Only stop the game via time when host selected max time as game mode
                     ServerActionHandler.triggerAction(Constants.PREFIX_END_GAME, "TIME RAN OUT");
                     timer.cancel();
                 }
@@ -64,7 +64,7 @@ public class RequestGameStartTime implements ServerActionInterface {
     }
 
     private void cancelPreviousTimer() {
-        elapsedTime = !Config.SELECTED_GAME_MODE? Config.END_TIME: 0;
+        elapsedTime = Config.IS_TIME_MODE ? Config.END_TIME : 0; // if is time mode -> ElapsedTime starts at end time
         previousMinute = -1;
 
         if (timer != null) {
