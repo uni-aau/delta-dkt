44a8e048e442fe01dca11abfba0707774de9396e
djamn <david@jamnig.net>
2023-06-13 19:08:43 +0200

    feat: added handling for boolean in ActionUpdateGameTime

--- app/src/main/java/ClientUIHandling/actions/ActionUpdateGameTime.java
+++ app/src/main/java/ClientUIHandling/actions/ActionUpdateGameTime.java
@@ -1,5 +1,6 @@
 package ClientUIHandling.actions;
 
+import static ClientUIHandling.Constants.GAMEVIEW_ACTIVITY_TYPE;
 import static ClientUIHandling.Constants.PREFIX_GET_SERVER_TIME;
 
 import android.widget.TextView;
@@ -8,31 +9,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import ClientUIHandling.ClientActionInterface;
 import delta.dkt.R;
+import delta.dkt.activities.GameViewActivity;
 
 public class ActionUpdateGameTime implements ClientActionInterface {
+    private AppCompatActivity gameViewActivity;
     @Override
     public void execute(AppCompatActivity activity, String clientMessage) {
         String[] args = clientMessage.replace(PREFIX_GET_SERVER_TIME, "").trim().split(";"); // Holt sich Args nach dem Prefix
+        gameViewActivity = (GameViewActivity) activity;
+        int time = Integer.parseInt(args[0]);
+        boolean isTimeMode = Boolean.parseBoolean(args[1]);
 
-        updateGameTime(args[0], activity);
+        updateGameTime(time, isTimeMode);
     }
 
     /**
      * This method updates the game time in the gameview (converts milliseconds to hours, minutes and seconds)
      */
-    private void updateGameTime(String args, AppCompatActivity activity) {
-        int milliseconds = Integer.parseInt(args);
-
+    private void updateGameTime(int milliseconds, boolean isTimeMode) {
         int hours = (milliseconds / (1000 * 60 * 60)) % 24;
         int minutes = (milliseconds / (1000 * 60)) % 60;
         int seconds = (milliseconds / 1000) % 60;
 
         // Sets the text for the playing time ( != 1 -> pluralText)
-        String hoursLocale = hours != 1 ? activity.getString(R.string.hours_plural_text) : activity.getString(R.string.hour_sing_text);
-        String minutesLocale = minutes != 1 ? activity.getString(R.string.minutes_plural_text) : activity.getString(R.string.minute_sing_text);
-        String secondsLocale = seconds != 1 ? activity.getString(R.string.seconds_plural_text) : activity.getString(R.string.second_sing_text);
+        String hoursLocale = hours != 1 ? gameViewActivity.getString(R.string.hours_plural_text) : gameViewActivity.getString(R.string.hour_sing_text);
+        String minutesLocale = minutes != 1 ? gameViewActivity.getString(R.string.minutes_plural_text) : gameViewActivity.getString(R.string.minute_sing_text);
+        String secondsLocale = seconds != 1 ? gameViewActivity.getString(R.string.seconds_plural_text) : gameViewActivity.getString(R.string.second_sing_text);
 
         // %d %s, %d %s, %d, %s
-        ((TextView) activity.findViewById(R.id.textView_playing_time)).setText(String.format(activity.getString(R.string.playing_time_text), String.valueOf(hours), hoursLocale, String.valueOf(minutes), minutesLocale, String.valueOf(seconds), secondsLocale));
+        ((TextView) gameViewActivity.findViewById(R.id.textView_playing_time)).setText(String.format(gameViewActivity.getString(R.string.playing_time_text), String.valueOf(hours), hoursLocale, String.valueOf(minutes), minutesLocale, String.valueOf(seconds), secondsLocale));
     }
 }
