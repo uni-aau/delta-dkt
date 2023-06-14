82639312947616308d36f27ace169de597bfc526
djamn <david@jamnig.net>
2023-06-14 15:24:36 +0200

    fix: reformatted RequestBuyProperty Wrong format

--- app/src/main/java/ServerLogic/actions/RequestBuyProperty.java
+++ app/src/main/java/ServerLogic/actions/RequestBuyProperty.java
@@ -15,35 +15,35 @@ import static ClientUIHandling.Constants.*;
 public class RequestBuyProperty implements ServerActionInterface {
     @Override
     public void execute(ServerNetworkClient server, Object parameters) {
-            Log.d("INFO", "BuyPropertyAction");
+        Log.d("INFO", "BuyPropertyAction");
 
-            if (! Game.getPlayers().containsKey((int)parameters)) {
-                Log.e("ERROR","The playerId provided to the BuyPropertyAction class does not exist. Id:  " + parameters.toString());
-                return;
-            }
+        if (!Game.getPlayers().containsKey((int) parameters)) {
+            Log.e("ERROR", "The playerId provided to the BuyPropertyAction class does not exist. Id:  " + parameters.toString());
+            return;
+        }
 
-            //fetch player object
-            int clientId = (int) parameters;
-            Player player = Game.getPlayers().get(clientId);
-            int fieldLocation = player.getPosition().getLocation();
-            Log.d(LOG_PROPERTY_BUY, "BuyProperty location: "+fieldLocation);
-            Field field = Game.getMap().getField(fieldLocation);
+        //fetch player object
+        int clientId = (int) parameters;
+        Player player = Game.getPlayers().get(clientId);
+        int fieldLocation = player.getPosition().getLocation();
+        Log.d(LOG_PROPERTY_BUY, "BuyProperty location: " + fieldLocation);
+        Field field = Game.getMap().getField(fieldLocation);
 
-            if(field instanceof Property && ((Property) field).getOwner() == null) {
-                Property property = (Property) field;
-                if(property.getPrice() <= player.getCash()) {
-                    boolean success = player.buyProperty(property.getLocation());
-                    if (success) {
-                        ServerActionHandler.triggerAction(PREFIX_PROPLIST_UPDATE, fieldLocation); // initializes property list
-                        server.broadcast(GAMEVIEW_ACTIVITY_TYPE, PREFIX_PLAYER_PROPERTYBOUGHT, new String[]{String.valueOf(clientId)});
-                        server.broadcast(GAMEVIEW_ACTIVITY_TYPE, PREFIX_ACTIVITY_BROADCAST, new String[]{"player_buy_property_activity_text", player.getNickname(), property.getName(), String.valueOf(property.getLocation()), String.valueOf(property.getPrice())});
-                        server.broadcast(GAMEVIEW_ACTIVITY_TYPE, PREFIX_SET_MONEY, new String[]{String.valueOf(clientId), String.valueOf(player.getCash())});
-                    }
-                } else {
-                    // TODO GELD REICHT NICHT AUS - Könnte auch direkt via RequestAskPropertyBuy klasse implementiert werden
+        if (field instanceof Property && ((Property) field).getOwner() == null) {
+            Property property = (Property) field;
+            if (property.getPrice() <= player.getCash()) {
+                boolean success = player.buyProperty(property.getLocation());
+                if (success) {
+                    ServerActionHandler.triggerAction(PREFIX_PROPLIST_UPDATE, fieldLocation); // initializes property list
+                    server.broadcast(GAMEVIEW_ACTIVITY_TYPE, PREFIX_PLAYER_PROPERTYBOUGHT, new String[]{String.valueOf(clientId)});
+                    server.broadcast(GAMEVIEW_ACTIVITY_TYPE, PREFIX_ACTIVITY_BROADCAST, new String[]{"player_buy_property_activity_text", player.getNickname(), property.getName(), String.valueOf(property.getLocation()), String.valueOf(property.getPrice())});
+                    server.broadcast(GAMEVIEW_ACTIVITY_TYPE, PREFIX_SET_MONEY, new String[]{String.valueOf(clientId), String.valueOf(player.getCash())});
                 }
             } else {
-                Log.e(LOG_PROPERTY_BUY, "Error trying to buy plot - Invalid arguments provided!");
+                // TODO GELD REICHT NICHT AUS - Könnte auch direkt via RequestAskPropertyBuy klasse implementiert werden
             }
+        } else {
+            Log.e(LOG_PROPERTY_BUY, "Error trying to buy plot - Invalid arguments provided!");
         }
     }
+}
