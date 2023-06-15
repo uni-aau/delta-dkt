b46c94959cba4218f25bb95d4ce75653a69d8bf8
djamn <david@jamnig.net>
2023-06-15 18:40:52 +0200

    fix: added CashRiskTask class that extends RiskTask

---
+++ app/src/main/java/delta/dkt/logic/structure/CashRiskTask.java
@@ -0,0 +1,30 @@
+package delta.dkt.logic.structure;
+
+import android.util.Log;
+
+import ClientUIHandling.Constants;
+import ServerLogic.ServerActionHandler;
+
+public class CashRiskTask extends RiskTask {
+    private final int cashAmount;
+
+    /**
+     * Creates a new risk task.
+     *
+     * @param id The id of the risk task
+     * @param name The name of the risk task
+     * @param descriptionString Tells you what task you have to do
+     * @param cashAmount Tells you about the cash / steps amount
+     */
+    public CashRiskTask(int id, String name, String descriptionString, int cashAmount) {
+        super(id, name, descriptionString);
+        this.cashAmount = cashAmount;
+    }
+
+    @Override
+    public void execute(Player assignee) {
+        Log.d("RiskTask", "Sending RiskTask execution to server");
+
+        ServerActionHandler.triggerAction(Constants.PREFIX_CASH_TASK, new String[]{String.valueOf(assignee.getId()), getDescriptionString(), String.valueOf(getId()), String.valueOf(cashAmount)});
+    }
+}
