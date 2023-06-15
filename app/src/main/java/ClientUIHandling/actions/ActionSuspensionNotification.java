dd8671bdd6b5eb1539c28e2da030c30165f0054b
djamn <david@jamnig.net>
2023-06-15 17:42:51 +0200

    feat: updated broadcast for jail so that only the specific person receives the toast No other person is interested in the fact that "The player" is still in prison

--- app/src/main/java/ClientUIHandling/actions/ActionSuspensionNotification.java
+++ app/src/main/java/ClientUIHandling/actions/ActionSuspensionNotification.java
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import ClientUIHandling.ClientActionInterface;
 import ClientUIHandling.handlers.languages.LanguageHandler;
 import delta.dkt.R;
+import delta.dkt.activities.GameViewActivity;
 
 public class ActionSuspensionNotification implements ClientActionInterface {
 
@@ -17,7 +18,10 @@ public class ActionSuspensionNotification implements ClientActionInterface {
     @Override
     public void execute(AppCompatActivity activity, String clientMessage) {
         String[] args = clientMessage.replace(PREFIX_SUSPENSION_COUNT, "").trim().split(";");
+        int clientID = Integer.parseInt(args[1]);
 
-        Toast.makeText(activity.getApplicationContext(), LanguageHandler.formatText(activity.getString(R.string.suspension_count), new Object[]{args[0]}), Toast.LENGTH_SHORT).show();
+        if(clientID == GameViewActivity.clientID) {
+            Toast.makeText(activity.getApplicationContext(), LanguageHandler.formatText(activity.getString(R.string.suspension_count), new Object[]{args[0]}), Toast.LENGTH_SHORT).show();
+        }
     }
 }
