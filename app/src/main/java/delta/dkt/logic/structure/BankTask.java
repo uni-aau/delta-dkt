822901706411aba2b9069ac2ace346ed54558ca2
djamn <david@jamnig.net>
2023-06-15 18:46:04 +0200

    fix: added comment in BankTask to fix code smell

--- app/src/main/java/delta/dkt/logic/structure/BankTask.java
+++ app/src/main/java/delta/dkt/logic/structure/BankTask.java
@@ -19,7 +19,7 @@ public class BankTask extends Task{
     }
 
     @Override
-    public void execute(Player asignee) {
-
+    public void execute(Player assignee) {
+        // Will be called in Subclasses
     }
 }
