1ee902bb8ff1563a3043a9754b91dad7bbf0606c
djamn <david@jamnig.net>
2023-06-15 21:18:50 +0200

    fix: added testcases for Bank and RiskTask

---
+++ app/src/test/java/delta/dkt/logic/TasksTest.java
@@ -0,0 +1,25 @@
+package delta.dkt.logic;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+import delta.dkt.logic.structure.BankTask;
+import delta.dkt.logic.structure.BankTaskField;
+import delta.dkt.logic.structure.RiskTask;
+import delta.dkt.logic.structure.RiskTaskField;
+
+public class TasksTest {
+    RiskTaskField riskTaskField = new RiskTaskField(1, new RiskTask(1, "Risktask", "Description"));
+    BankTaskField bankTaskField = new BankTaskField(2, new BankTask(2, "BankTask", "Description"));
+
+    @Test
+    void testRiskTaskFieldNotNull() {
+        assertNotNull(riskTaskField.getRiskTask());
+    }
+
+    @Test
+    void testBankTaskFieldNotNull() {
+        assertNotNull(bankTaskField.getBankTask());
+    }
+}
