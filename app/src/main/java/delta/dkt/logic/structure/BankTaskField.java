b31e1cebc27c4d8a3aa79fd754f60f988c754b10
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-06-16 14:10:36 +0200

    feat: removed Task argument - BankTaskField

--- app/src/main/java/delta/dkt/logic/structure/BankTaskField.java
+++ app/src/main/java/delta/dkt/logic/structure/BankTaskField.java
@@ -4,20 +4,15 @@ package delta.dkt.logic.structure;
  * This class represents a bank task field
  */
 public class BankTaskField extends Field{
-
-    private final BankTask bankTask;
-
     /**
      * Creates a new bank task.
      * @param location location of the bank task field
-     * @param bankTask the bank task that the player has to do
      */
-    public BankTaskField(int location, BankTask bankTask) {
+    public BankTaskField(int location) {
         super(location);
-        this.bankTask = bankTask;
     }
 
     public BankTask getBankTask() {
-        return bankTask;
+        return (BankTask) TaskHandler.getTask(this.getLocation());
     }
 }
