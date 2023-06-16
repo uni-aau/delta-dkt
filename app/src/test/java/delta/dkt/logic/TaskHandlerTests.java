2927d4029f82df8abe6a955688c5cbfbc49d8b4c
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-06-16 16:04:28 +0200

    fix: mocked getFields method - TaskHandlerTests
    Sometimes the test failed, because the GameMap had returned an empty list of fields. Therefore, the GameMap has been mocked to return either a RiskTaskField or BankTaskField for the requested field-positions.

--- app/src/test/java/delta/dkt/logic/TaskHandlerTests.java
+++ app/src/test/java/delta/dkt/logic/TaskHandlerTests.java
@@ -2,13 +2,21 @@ package delta.dkt.logic;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.*;
 
+import delta.dkt.logic.structure.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import delta.dkt.logic.structure.TaskHandler;
-
 class TaskHandlerTests {
+
+    private GameMap mockMap = mock(GameMap.class);
+
+    @BeforeEach
+    void setup(){
+        Game.setMap(mockMap);
+    }
 
     /**
      * Check if the getTask methode returns not null if it gets a location where there is a task field.
@@ -16,7 +24,16 @@ class TaskHandlerTests {
     @ParameterizedTest
     @ValueSource(ints = {3, 9, 23, 28, 38})
     void testGetTaskExistingTask(int input){
+        when(mockMap.getField(3)).thenReturn(new RiskTaskField(3));
+        when(mockMap.getField(23)).thenReturn(new RiskTaskField(23));
+        when(mockMap.getField(38)).thenReturn(new RiskTaskField(38));
+
+        when(mockMap.getField(9)).thenReturn(new BankTaskField(9));
+        when(mockMap.getField(28)).thenReturn(new BankTaskField(28));
+
         assertNotNull(TaskHandler.getTask(input));
+
+        verify(mockMap, atLeastOnce()).getField(input);
     }
 
     /**
