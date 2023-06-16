5b44a02bdb0a4cf50eedf995dbd04086bc6c8889
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-06-16 13:23:43 +0200

    feat: simplified getTask method - TaskHandler

--- app/src/main/java/delta/dkt/logic/structure/TaskHandler.java
+++ app/src/main/java/delta/dkt/logic/structure/TaskHandler.java
@@ -13,18 +13,12 @@ public class TaskHandler {
      * Gets a random task out of the BankTaskCollection/RiskTaskCollection of a given position
      * @param position The position of the property.
      */
-    public static Field getTask(int position){
-
+    public static Task getTask(int position){
         RiskTaskCollection r = new RiskTaskCollection();
         BankTaskCollection b = new BankTaskCollection();
 
-        switch (position){
-            case 3: return new RiskTaskField(3,r.getRandomRiskTask());
-            case 9: return new BankTaskField(9,b.getRandomBankTask());
-            case 23: return new RiskTaskField(23,r.getRandomRiskTask());
-            case 28: return new BankTaskField(28,b.getRandomBankTask());
-            case 38: return new RiskTaskField(38,r.getRandomRiskTask());
-            default: return null;
-        }
+        if(Game.getMap().getField(position) instanceof BankTaskField) return b.getRandomBankTask();
+
+        return r.getRandomRiskTask();
     }
 }
